f18cb26d58517811e687926785ba839923e42024
Roberto <rpgrca@gmail.com>
2023-12-02 09:30:25 -0300

    add test, parse one game correctly

--- 2023/Day2/Day2.Logic/CubeConundrumGame.cs
+++ 2023/Day2/Day2.Logic/CubeConundrumGame.cs
@@ -1,4 +1,5 @@
 
+
 namespace Day2.Logic;
 
 public class CubeConundrumGame
@@ -6,11 +7,41 @@ public class CubeConundrumGame
     private readonly string _input;
     private readonly string[] _games;
 
-    public int Games => _games.Length;
+    public int GameCount => _games.Length;
+
+    public List<(int Blue, int Red, int Green)> Games { get; private set; }
 
     public CubeConundrumGame(string input)
     {
         _input = input;
         _games = _input.Split("\n");
+        Games = new List<(int Blue, int Red, int Green)>();
+
+        Parse();
+    }
+
+    private void Parse()
+    {
+        var fields = _input.Split(":");
+        var games = fields[1].Split(";");
+
+        foreach (var game in games)
+        {
+            var values = game.Trim().Split(",");
+
+            (int Blue, int Red, int Green) cubes = (0, 0, 0);
+            foreach (var value in values)
+            {
+                var data = value.Trim().Split(" ");
+                switch (data[1].Trim())
+                {
+                    case "red": cubes.Red = int.Parse(data[0].Trim()); break;
+                    case "blue": cubes.Blue = int.Parse(data[0].Trim()); break;
+                    case "green": cubes.Green = int.Parse(data[0].Trim()); break;
+                }
+            }
+
+            Games.Add(cubes);
+        }
     }
 }
--- 2023/Day2/Day2.UnitTests/CubeConundrumGameMust.cs
+++ 2023/Day2/Day2.UnitTests/CubeConundrumGameMust.cs
@@ -11,6 +11,19 @@ public class CubeConundrumGameMust
     public void LoadInputCorrectly(string input, int expectedLength)
     {
         var sut = new CubeConundrumGame(input);
-        Assert.Equal(expectedLength, sut.Games);
+        Assert.Equal(expectedLength, sut.GameCount);
     }
+
+    [Fact]
+    public void ParseInputCorrectly()
+    {
+        var sut = new CubeConundrumGame("Game 1: 3 blue, 4 red");
+        Assert.Collection(sut.Games,
+            p1 => {
+                Assert.Equal(3, p1.Blue);
+                Assert.Equal(4, p1.Red);
+                Assert.Equal(0, p1.Green);
+            });
+    }
+
 }
