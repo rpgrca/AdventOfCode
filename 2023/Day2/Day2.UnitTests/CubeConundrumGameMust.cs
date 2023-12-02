0dab7bf0f5e5c50aff9f24b8e4f166db62c935dd
Roberto <rpgrca@gmail.com>
2023-12-02 09:18:39 -0300

    add test, load puzzle data correctly

--- 2023/Day2/Day2.UnitTests/CubeConundrumGameMust.cs
+++ 2023/Day2/Day2.UnitTests/CubeConundrumGameMust.cs
@@ -5,10 +5,12 @@ namespace Day2.UnitTests;
 
 public class CubeConundrumGameMust
 {
-    [Fact]
-    public void LoadInputCorrectly()
+    [Theory]
+    [InlineData(SAMPLE_INPUT, 5)]
+    [InlineData(PUZZLE_INPUT, 100)]
+    public void LoadInputCorrectly(string input, int expectedLength)
     {
-        var sut = new CubeConundrumGame(SAMPLE_INPUT);
-        Assert.Equal(5, sut.Games);
+        var sut = new CubeConundrumGame(input);
+        Assert.Equal(expectedLength, sut.Games);
     }
 }
