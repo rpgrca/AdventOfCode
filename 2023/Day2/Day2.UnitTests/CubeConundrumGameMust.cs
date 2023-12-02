c836e4c7425fc4e2abba278eed80a0a5ceefc419
Roberto <rpgrca@gmail.com>
2023-12-02 10:15:23 -0300

    solve second sample correctly

--- 2023/Day2/Day2.UnitTests/CubeConundrumGameMust.cs
+++ 2023/Day2/Day2.UnitTests/CubeConundrumGameMust.cs
@@ -116,4 +116,11 @@ Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue");
         var sut = new CubeConundrumGame("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green");
         Assert.Equal(48, sut.SumOfPowers);
     }
+
+    [Fact]
+    public void SolveSecondSampleCorrectly()
+    {
+        var sut = new CubeConundrumGame(SAMPLE_INPUT);
+        Assert.Equal(2286, sut.SumOfPowers);
+    }
 }
