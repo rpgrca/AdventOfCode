93dc515c88d3d1d46d367f3d07d2ad5770aae7a9
Roberto <rpgrca@gmail.com>
2023-12-04 09:04:15 -0300

    add test, parse input correctly

--- 2023/Day4/Day4.Logic/Scratchcards.cs
+++ 2023/Day4/Day4.Logic/Scratchcards.cs
@@ -1,5 +1,43 @@
+
 namespace Day4.Logic;
 public class Scratchcards
 {
+    private readonly string _input;
+    private readonly string[] _scratchcards;
+    private readonly List<int[]> _winningValues;
+    private readonly List<int[]> _ownedValues;
 
+    public int Amount => _scratchcards.Length;
+    public int WinningAmount => _winningValues[0].Length;
+    public int OwnedAmount => _ownedValues[0].Length;
+
+    public Scratchcards(string input)
+    {
+        _input = input;
+        _winningValues = new List<int[]>();
+        _ownedValues = new List<int[]>();
+        _scratchcards = _input.Split("\n");
+
+        Parse();
+    }
+
+    private void Parse()
+    {
+        foreach (var line in _scratchcards)
+        {
+            var splittedLine = line.Split(":");
+            var amounts = splittedLine[1].Split("|");
+            _winningValues.Add(amounts[0]
+                .Split(" ")
+                .Where(p => !string.IsNullOrEmpty(p))
+                .Select(int.Parse)
+                .ToArray());
+
+            _ownedValues.Add(amounts[1]
+                .Split(" ")
+                .Where(p => !string.IsNullOrEmpty(p))
+                .Select(int.Parse)
+                .ToArray());
+        }
+    }
 }
--- 2023/Day4/Day4.UnitTests/ScratchcardsMust.cs
+++ 2023/Day4/Day4.UnitTests/ScratchcardsMust.cs
@@ -5,9 +5,14 @@ namespace Day4.UnitTests;
 
 public class ScratchcardsMust
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData(SAMPLE_INPUT, 6, 5, 8)]
+    [InlineData(PUZZLE_INPUT, 223, 10, 25)]
+    public void LoadInputCorrectly(string input, int expectedAmount, int expectedWinningNumbers, int expectedOwnedNumbers)
     {
-
+        var sut = new Scratchcards(input);
+        Assert.Equal(expectedAmount, sut.Amount);
+        Assert.Equal(expectedWinningNumbers, sut.WinningAmount);
+        Assert.Equal(expectedOwnedNumbers, sut.OwnedAmount);
     }
 }
