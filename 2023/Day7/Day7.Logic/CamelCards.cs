3b83622f563998ba7a47c0fdcaba0d151e6dee7e
Roberto <rpgrca@gmail.com>
2023-12-07 09:16:52 -0300

    refactor, split lines in hand and bid

--- 2023/Day7/Day7.Logic/CamelCards.cs
+++ 2023/Day7/Day7.Logic/CamelCards.cs
@@ -4,13 +4,25 @@ public class CamelCards
 {
     private readonly string _input;
     private readonly string[] _lines;
+    private readonly List<(string, int)> _hands;
 
-    public int Hands => _lines.Length;
+    public int Hands => _hands.Count;
 
     public CamelCards(string input)
     {
         _input = input;
         _lines = input.Split("\n");
+        _hands = new List<(string, int)>();
+
+        Parse();
     }
 
+    private void Parse()
+    {
+        foreach (var line in _lines)
+        {
+            var splittedLine = line.Split(" ");
+            _hands.Add((splittedLine[0].Trim(), int.Parse(splittedLine[1])));
+        }
+    }
 }
