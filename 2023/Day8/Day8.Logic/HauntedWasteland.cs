691fb49828fbbc92a77304b9b0d7296b9a1d4736
Roberto <rpgrca@gmail.com>
2023-12-08 12:18:37 -0300

    simplify code

--- 2023/Day8/Day8.Logic/HauntedWasteland.cs
+++ 2023/Day8/Day8.Logic/HauntedWasteland.cs
@@ -9,11 +9,6 @@ public class HauntedWasteland
 {
     private readonly string _input;
     private int _currentIndex;
-    private Dictionary<int, Dictionary<char, int>> _numericStates;
-    public int[] _currentNumericStates;
-    private int[] _finalStates;
-    private int _startingState;
-    private int _endingState;
 
     public int StateCount { get; private set; }
     public int InstructionCount { get; private set; }
@@ -25,9 +20,6 @@ public class HauntedWasteland
     public HauntedWasteland(string input, bool forGhosts = false)
     {
         _input = input;
-        _numericStates = new Dictionary<int, Dictionary<char, int>>();
-        _currentNumericStates = Array.Empty<int>();
-        _finalStates = Array.Empty<int>();
         Instructions = string.Empty;
         Steps = new Dictionary<string, Dictionary<char, string>>();
         CurrentStates = Array.Empty<string>();
@@ -36,8 +28,7 @@ public class HauntedWasteland
 
         if (!forGhosts)
         {
-            //CalculateStepsToGoal();
-            CalculateStepsToGoalWithNumericStates();
+            CalculateStepsToGoal();
         }
     }
 
@@ -45,9 +36,6 @@ public class HauntedWasteland
     {
         var lines = _input.Split("\n");
         var currentStates = new List<string>();
-        var finalStates = new List<int>();
-        var dictionary = new Dictionary<string, int>();
-        var currentNumericStates = new List<int>();
 
         Instructions = lines[0];
         InstructionCount = lines[0].Length;
@@ -59,9 +47,6 @@ public class HauntedWasteland
             var key = values[0].Trim();
 
             Steps.Add(key, new Dictionary<char, string>());
-            var oldIndex = dictionary.Count;
-            dictionary.Add(key, oldIndex);
-            _numericStates.Add(dictionary[key], new Dictionary<char, int>());
 
             var branches = values[1].Trim()[1..^1].Split(",");
             Steps[key].Add('L', branches[0].Trim());
@@ -69,35 +54,11 @@ public class HauntedWasteland
 
             if (key[^1] == 'A')
             {
-                if (key == "AAA")
-                {
-                    _startingState = dictionary[key];
-                }
-
                 currentStates.Add(key);
-                currentNumericStates.Add(dictionary[key]);
             }
-
-            if (key[^1] == 'Z')
-            {
-                if (key == "ZZZ")
-                {
-                    _endingState = dictionary[key];
-                }
-
-                finalStates.Add(dictionary[key]);
-            }
-        }
-
-        foreach (var entry in Steps)
-        {
-            _numericStates[dictionary[entry.Key]].Add('L', dictionary[entry.Value['L']]);
-            _numericStates[dictionary[entry.Key]].Add('R', dictionary[entry.Value['R']]);
         }
 
         CurrentStates = currentStates.ToArray();
-        _finalStates = finalStates.ToArray();
-        _currentNumericStates = currentNumericStates.ToArray();
     }
 
     private void CalculateStepsToGoal()
@@ -118,27 +79,6 @@ public class HauntedWasteland
         }
     }
 
-    private void CalculateStepsToGoalWithNumericStates()
-    {
-        var index = 0;
-        StepsToReachGoal = 0;
-
-        while (_startingState != _endingState)
-        {
-            if (StepsToReachGoal > 21407)
-            {
-                System.Diagnostics.Debugger.Break();
-            }
-            _startingState = _numericStates[_startingState][Instructions[index]];
-            StepsToReachGoal++;
-            index++;
-            if (index >= InstructionCount)
-            {
-                index = 0;
-            }
-        }
-   }
-
     public void Step()
     {
         for (var index = 0; index < CurrentStates.Length; index++)
@@ -154,26 +94,17 @@ public class HauntedWasteland
         }
     }
 
-    public void ReachGoalInGhostMode()
-    {
-        while (CurrentStates.Any(p => p[^1] != 'Z'))
-        {
-            Step();
-        }
-    }
-
     public void ReachGoalInGhostModeWithNumericStates()
     {
         var stepsToFinalState = new List<int>();
         var currentInstruction = 0;
-        for (var index = 0; index < _currentNumericStates.Length; index++)
+        for (var index = 0; index < CurrentStates.Length; index++)
         {
-            var currentState = _currentNumericStates[index];
             var legCount = 0;
 
-            while (! _finalStates.Contains(currentState))
+            while (CurrentStates[index][^1] != 'Z')
             {
-                currentState = _numericStates[currentState][Instructions[currentInstruction++]];
+                CurrentStates[index] = Steps[CurrentStates[index]][Instructions[currentInstruction++]];
                 legCount++;
 
                 if (currentInstruction >= InstructionCount)
