9c70cf3a179f92ed8f6502b465f9be32f424c643
Roberto <rpgrca@gmail.com>
2023-12-09 10:29:09 -0300

    add test, parse input correctly

--- 2023/Day9/Day9.Logic/MirageMaintenance.cs
+++ 2023/Day9/Day9.Logic/MirageMaintenance.cs
@@ -1,14 +1,24 @@
 
+
 namespace Day9.Logic;
 public class MirageMaintenance
 {
     private readonly string _input;
+    private readonly string[] _lines;
 
     public int HistoryCount { get; set; }
+    public List<int[]> Histories { get; private set; }
 
     public MirageMaintenance(string input)
     {
         _input = input;
-        HistoryCount = _input.Split("\n").Length;
+        _lines = _input.Split("\n");
+
+        HistoryCount = _lines.Length;
+        Histories = new List<int[]>();
+        foreach (var line in _lines)
+        {
+            Histories.Add(line.Split(" ").Select(int.Parse).ToArray());
+        }
     }
 }
--- 2023/Day9/Day9.UnitTests/MirageMaintenanceMust.cs
+++ 2023/Day9/Day9.UnitTests/MirageMaintenanceMust.cs
@@ -1,4 +1,5 @@
 using Day9.Logic;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Host;
 using static Day9.UnitTests.Constants;
 
 namespace Day9.UnitTests;
@@ -12,5 +13,15 @@ public class MirageMaintenanceMust
     {
         var sut = new MirageMaintenance(input);
         Assert.Equal(expectedHistoryCount, sut.HistoryCount);
+    }
+
+    [Fact]
+    public void ParseInputCorrectly()
+    {
+        var sut = new MirageMaintenance(SAMPLE_INPUT);
+        Assert.Collection(sut.Histories,
+            p1 => Assert.Equal(new[] { 0, 3, 6, 9, 12, 15 }, p1),
+            p2 => Assert.Equal(new[] { 1, 3, 6, 10, 15, 21 }, p2),
+            p3 => Assert.Equal(new[] { 10, 13, 16, 21, 30, 45 }, p3));
     }
 }
