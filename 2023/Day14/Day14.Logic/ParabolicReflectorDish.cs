a54403fa64ca0e033efa8b5970a329c050c7dbea
Roberto <rpgrca@gmail.com>
2023-12-14 08:18:37 -0300

    add test, initial setup

---
+++ 2023/Day14/Day14.Logic/ParabolicReflectorDish.cs
@@ -0,0 +1,16 @@
+
+namespace Day14.Logic;
+
+public class ParabolicReflectorDish
+{
+    private readonly string _input;
+
+    public int Height { get; private set; } = 10;
+    public int Width { get; private set; } = 10;
+
+    public ParabolicReflectorDish(string input)
+    {
+        _input = input;
+    }
+
+}
---
+++ 2023/Day14/Day14.UnitTests/ParabolicReflectorDishMust.cs
@@ -0,0 +1,16 @@
+using Day14.Logic;
+using static Day14.UnitTests.Constants;
+
+namespace Day14.UnitTests;
+
+public class ParabolicReflectorDishMust
+{
+    [Theory]
+    [InlineData(SAMPLE_INPUT, 10, 10)]
+    public void LoadInputCorrectly(string input, int expectedWidth, int expectedHeight)
+    {
+        var sut = new ParabolicReflectorDish(input);
+        Assert.Equal(expectedWidth, sut.Width);
+        Assert.Equal(expectedHeight, sut.Height);
+    }
+}
