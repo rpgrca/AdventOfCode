6ade9d7c0af365b281a4b6d51617f52dc409ca3d
Roberto <rpgrca@gmail.com>
2023-12-15 10:36:18 -0300

    add extra tests for extra steps

--- 2023/Day15/Day15.UnitTests/LensLibraryMust.cs
+++ 2023/Day15/Day15.UnitTests/LensLibraryMust.cs
@@ -1,3 +1,4 @@
+using System.Collections.Specialized;
 using System.Runtime.Intrinsics.X86;
 using Day15.Logic;
 using static Day15.UnitTests.Constants;
@@ -94,4 +95,80 @@ public class LensLibraryMust
             b2 => Assert.Equal(("cm", 2), b2));
         Assert.Empty(sut.Boxes[1]);
     }
+
+    [Fact]
+    public void SelectBoxCorrectly_WhenExecuting6Steps()
+    {
+        var sut = new LensLibrary("rn=1,cm-,qp=3,cm=2,qp-,pc=4");
+        Assert.Collection(sut.Boxes[0],
+            b1 => Assert.Equal(("rn", 1), b1),
+            b2 => Assert.Equal(("cm", 2), b2));
+        Assert.Collection(sut.Boxes[3],
+            b1 => Assert.Equal(("pc", 4), b1));
+    }
+
+    [Fact]
+    public void SelectBoxCorrectly_WhenExecuting7Steps()
+    {
+        var sut = new LensLibrary("rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9");
+        Assert.Collection(sut.Boxes[0],
+            b1 => Assert.Equal(("rn", 1), b1),
+            b2 => Assert.Equal(("cm", 2), b2));
+        Assert.Collection(sut.Boxes[3],
+            b1 => Assert.Equal(("pc", 4), b1),
+            b2 => Assert.Equal(("ot", 9), b2));
+    }
+
+    [Fact]
+    public void SelectBoxCorrectly_WhenExecuting8Steps()
+    {
+        var sut = new LensLibrary("rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5");
+        Assert.Collection(sut.Boxes[0],
+            b1 => Assert.Equal(("rn", 1), b1),
+            b2 => Assert.Equal(("cm", 2), b2));
+        Assert.Collection(sut.Boxes[3],
+            b1 => Assert.Equal(("pc", 4), b1),
+            b2 => Assert.Equal(("ot", 9), b2),
+            b3 => Assert.Equal(("ab", 5), b3));
+    }
+
+    [Fact]
+    public void SelectBoxCorrectly_WhenExecuting9Steps()
+    {
+        var sut = new LensLibrary("rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-");
+        Assert.Collection(sut.Boxes[0],
+            b1 => Assert.Equal(("rn", 1), b1),
+            b2 => Assert.Equal(("cm", 2), b2));
+        Assert.Collection(sut.Boxes[3],
+            b1 => Assert.Equal(("ot", 9), b1),
+            b2 => Assert.Equal(("ab", 5), b2));
+    }
+
+    [Fact]
+    public void SelectBoxCorrectly_WhenExecuting10Steps()
+    {
+        var sut = new LensLibrary("rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6");
+        Assert.Collection(sut.Boxes[0],
+            b1 => Assert.Equal(("rn", 1), b1),
+            b2 => Assert.Equal(("cm", 2), b2));
+        Assert.Collection(sut.Boxes[3],
+            b1 => Assert.Equal(("ot", 9), b1),
+            b2 => Assert.Equal(("ab", 5), b2),
+            b3 => Assert.Equal(("pc", 6), b3));
+    }
+
+    [Fact]
+    public void SelectBoxCorrectly_WhenExecuting11Steps()
+    {
+        var sut = new LensLibrary("rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7");
+        Assert.Collection(sut.Boxes[0],
+            b1 => Assert.Equal(("rn", 1), b1),
+            b2 => Assert.Equal(("cm", 2), b2));
+        Assert.Collection(sut.Boxes[3],
+            b1 => Assert.Equal(("ot", 7), b1),
+            b2 => Assert.Equal(("ab", 5), b2),
+            b3 => Assert.Equal(("pc", 6), b3));
+    }
+
+
 }
