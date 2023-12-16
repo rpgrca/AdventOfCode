0b8dc180141487e787d0afb52f81bfabfc7d59e2
Roberto <rpgrca@gmail.com>
2023-12-16 09:56:07 -0300

    add test, load input correctly

--- 2023/Day16/Day16.Logic/TheFloorWillBeLava.cs
+++ 2023/Day16/Day16.Logic/TheFloorWillBeLava.cs
@@ -1,5 +1,17 @@
+
 namespace Day16.Logic;
 public class TheFloorWillBeLava
 {
+    private readonly string _input;
+    private readonly string[] _lines;
+
+    public int Width => _lines[0].Length;
+    public int Height => _lines.Length;
+
+    public TheFloorWillBeLava(string input)
+    {
+        _input = input;
+        _lines = _input.Split("\n");
+    }
 
 }
--- 2023/Day16/Day16.UnitTests/TheFloorWillBeLavaMust.cs
+++ 2023/Day16/Day16.UnitTests/TheFloorWillBeLavaMust.cs
@@ -3,11 +3,15 @@ using static Day16.UnitTests.Constants;
 
 namespace Day16.UnitTests;
 
-public class TheFloorWillBeLava
+public class TheFloorWillBeLavaMust
 {
-    [Fact]
-    public void LoadInputCorrectly()
+    [Theory]
+    [InlineData(SAMPLE_INPUT, 10, 10)]
+    [InlineData(PUZZLE_INPUT, 110, 110)]
+    public void LoadInputCorrectly(string input, int expectedWidth, int expectedHeight)
     {
-
+        var sut = new TheFloorWillBeLava(input);
+        Assert.Equal(expectedWidth, sut.Width);
+        Assert.Equal(expectedHeight, sut.Height);
     }
 }
