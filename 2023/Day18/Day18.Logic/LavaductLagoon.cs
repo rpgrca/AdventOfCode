ca469ff8201a4a1db1fd1ef43931ec41967a89f1
Roberto <rpgrca@gmail.com>
2023-12-18 11:45:08 -0300

    add test, calculate area correctly

--- 2023/Day18/Day18.Logic/LavaductLagoon.cs
+++ 2023/Day18/Day18.Logic/LavaductLagoon.cs
@@ -1,3 +1,4 @@
+using System.Data;
 using System.Runtime.CompilerServices;
 
 namespace Day18.Logic;
@@ -8,11 +9,13 @@ public class LavaductLagoon
     private readonly string[] _lines;
     private readonly char[][] _map;
     private readonly int _length;
-
+    private readonly int _initialX;
+    private readonly int _initialY;
 
     public int DigPlanLength => _lines.Length;
 
     public int TrenchPerimeter { get; private set; }
+    public int TrenchArea { get; private set; }
 
     public LavaductLagoon(string input, int length = 100)
     {
@@ -20,6 +23,8 @@ public class LavaductLagoon
         _lines = _input.Split("\n");
         _map = new char[length][];
         _length = length;
+        _initialX = _length / 3;
+        _initialY = _length / 3;
 
         for (var index = 0; index < _length; index++)
         {
@@ -30,8 +35,8 @@ public class LavaductLagoon
     public void Dig()
     {
         int steps;
-        var currentX = _length / 3;
-        var currentY = _length / 3;
+        var currentX = _initialX;
+        var currentY = _initialY;
 
         foreach (var line in _lines)
         {
@@ -74,6 +79,22 @@ public class LavaductLagoon
             }
         }
 
+        CalculatePerimeter();
+        Draw();
+        CalculateArea();
+        Draw();
+    }
+
+    public void Draw()
+    {
+        foreach (var row in _map)
+        {
+            Console.WriteLine(string.Join("", row.Select(c => c)));
+        }
+    }
+
+    private void CalculatePerimeter()
+    {
         TrenchPerimeter = 0;
         for (var y = 0; y < _length; y++)
         {
@@ -86,4 +107,48 @@ public class LavaductLagoon
             }
         }
     }
+
+    private void CalculateArea()
+    {
+        FillArea(_initialX + 1, _initialY + 1);
+        TrenchArea = 0;
+        for (var y = 0; y < _length; y++)
+        {
+            for (var x = 0; x < _length; x++)
+            {
+                if (_map[y][x] == '#')
+                {
+                    TrenchArea++;
+                }
+            }
+        }
+    }
+
+    private void FillArea(int x, int y)
+    {
+        if (_map[y][x] == '.')
+        {
+            _map[y][x] = '#';
+            if (x + 1 < _length)
+            {
+                FillArea(x + 1, y); // r
+            }
+
+            if (x - 1 >= 0)
+            {
+                FillArea(x - 1, y); // l
+            }
+
+            if (y + 1 < _length)
+            {
+                FillArea(x, y + 1); // d
+            }
+
+            if (y - 1 >= 0)
+            {
+                FillArea(x, y - 1); // u
+            }
+        }
+    }
+
 }
--- 2023/Day18/Day18.UnitTests/LavaductLagoonMust.cs
+++ 2023/Day18/Day18.UnitTests/LavaductLagoonMust.cs
@@ -5,6 +5,7 @@ namespace Day18.UnitTests;
 
 public class LavaductLagoonMust
 {
+    /*
     [Theory]
     [InlineData(SAMPLE_INPUT, 14)]
     [InlineData(PUZZLE_INPUT, 728)]
@@ -16,11 +17,25 @@ public class LavaductLagoonMust
 
     [Theory]
     [InlineData("R 1 (#70c710)", 1)]
-    [InlineData("R 6 (#70c710)\nD 5 (#70c710)\nL 5 (#70c710)\nU 4 (#70c710)", 20)]
-    public void DigCorrectly(string input, int expectedPerimeter)
+    [InlineData("R 5 (#70c710)\nD 5 (#70c710)\nL 5 (#70c710)\nU 5 (#70c710)", 20)]
+    [InlineData(SAMPLE_INPUT, 38)]
+    [InlineData(PUZZLE_INPUT, 3770)]
+    public void CalculatePerimeterCorrectly(string input, int expectedPerimeter)
     {
-        var sut = new LavaductLagoon(input);
+        var sut = new LavaductLagoon(input, 700);
         sut.Dig();
         Assert.Equal(expectedPerimeter, sut.TrenchPerimeter);
+    }
+*/
+    [Theory]
+    //[InlineData("R 1 (#70c710)", 1)]
+    //[InlineData("R 5 (#70c710)\nD 5 (#70c710)\nL 5 (#70c710)\nU 5 (#70c710)", 36)]
+    [InlineData(SAMPLE_INPUT, 62)]
+    public void CalculateAreaCorrectly(string input, int expectedArea)
+    {
+        var sut = new LavaductLagoon(input, 80);
+        sut.Dig();
+        sut.Draw();
+        Assert.Equal(expectedArea, sut.TrenchArea);
     }
 }
