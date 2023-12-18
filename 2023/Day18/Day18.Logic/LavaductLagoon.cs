2d0af8f2a71c6823e893effb55afb50b94f6caf0
Roberto <rpgrca@gmail.com>
2023-12-18 10:29:47 -0300

    add test, load input correctly

--- 2023/Day18/Day18.Logic/LavaductLagoon.cs
+++ 2023/Day18/Day18.Logic/LavaductLagoon.cs
@@ -4,11 +4,13 @@ namespace Day18.Logic;
 public class LavaductLagoon
 {
     private readonly string _input;
+    private readonly string[] _lines;
 
     public LavaductLagoon(string input)
     {
         _input = input;
+        _lines = _input.Split("\n");
     }
 
-    public int DigPlanLength { get; set; } = 14;
+    public int DigPlanLength => _lines.Length;
 }
--- 2023/Day18/Day18.UnitTests/LavaductLagoonMust.cs
+++ 2023/Day18/Day18.UnitTests/LavaductLagoonMust.cs
@@ -7,6 +7,7 @@ public class LavaductLagoonMust
 {
     [Theory]
     [InlineData(SAMPLE_INPUT, 14)]
+    [InlineData(PUZZLE_INPUT, 728)]
     public void LoadInputCorrectly(string input, int expectedLength)
     {
         var sut = new LavaductLagoon(input);
