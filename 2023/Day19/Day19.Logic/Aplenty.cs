7d448d66ddeb397561632fec823bd0783a5e5706
Roberto <rpgrca@gmail.com>
2023-12-19 10:04:49 -0300

    add test, load input correctly

--- 2023/Day19/Day19.Logic/Aplenty.cs
+++ 2023/Day19/Day19.Logic/Aplenty.cs
@@ -1,6 +1,41 @@
+
 namespace Day19.Logic;
 
 public class Aplenty
 {
+    private readonly string _input;
+    private readonly string[] _lines;
+    private readonly List<string> _rules;
+    private readonly List<string> _parts;
 
+    public Aplenty(string input)
+    {
+        _input = input;
+        _lines = _input.Split("\n");
+        _rules = new List<string>();
+        _parts = new List<string>();
+
+        var inRules = true;
+        foreach (var line in _lines)
+        {
+            if (string.IsNullOrEmpty(line))
+            {
+                inRules = false;
+            }
+            else
+            {
+                if (inRules)
+                {
+                    _rules.Add(line);
+                }
+                else
+                {
+                    _parts.Add(line);
+                }
+            }
+        }
+    }
+
+    public int PartCount => _parts.Count;
+    public int RuleCount => _rules.Count;
 }
--- 2023/Day19/Day19.UnitTests/AplentyMust.cs
+++ 2023/Day19/Day19.UnitTests/AplentyMust.cs
@@ -5,9 +5,13 @@ namespace Day19.UnitTests;
 
 public class AplentyMust
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData(SAMPLE_INPUT, 11, 5)]
+    [InlineData(PUZZLE_INPUT, 587, 200)]
+    public void LoadDataCorrectly(string input, int expectedRuleCount, int expectedPartCount)
     {
-
+        var sut = new Aplenty(input);
+        Assert.Equal(expectedRuleCount, sut.RuleCount);
+        Assert.Equal(expectedPartCount, sut.PartCount);
     }
 }
