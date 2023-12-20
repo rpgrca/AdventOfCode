743a9130b57613dad9d64bfc0ca687390a9a9ee1
Roberto <rpgrca@gmail.com>
2023-12-20 10:54:01 -0300

    add test, parse input correctly

--- 2023/Day20/Day20.Logic/PulsePropagation.cs
+++ 2023/Day20/Day20.Logic/PulsePropagation.cs
@@ -1,4 +1,5 @@
 
+
 namespace Day20.Logic;
 
 public class PulsePropagation
@@ -10,7 +11,43 @@ public class PulsePropagation
     {
         _input = input;
         _lines = _input.Split("\n");
+
+        var flipflops = new HashSet<string>();
+        var conjuntions = new HashSet<string>();
+
+        foreach (var line in _lines)
+        {
+            var command = line.Split(" -> ");
+            if (command[0][0] == '%')
+            {
+                FlipFlopCount++;
+                flipflops.Add(command[0][1..]);
+            }
+            else
+            if (command[0][0] == '&')
+            {
+                ConjuntionCount++;
+                conjuntions.Add(command[0][1..]);
+            }
+        }
+
+        foreach (var line in _lines)
+        {
+            var command = line.Split(" -> ");
+            var targets = command[1].Split(",");
+            foreach (var target in targets.Select(p => p.Trim()))
+            {
+                if (!flipflops.Contains(target) && !conjuntions.Contains(target))
+                {
+                    UnnamedCount++;
+                }
+            }
+        }
     }
 
     public int CommandsCount => _lines.Length;
+
+    public int FlipFlopCount { get; set; }
+    public int ConjuntionCount { get; set; }
+    public int UnnamedCount { get; set; }
 }
--- 2023/Day20/Day20.UnitTests/PulsePropagationMust.cs
+++ 2023/Day20/Day20.UnitTests/PulsePropagationMust.cs
@@ -14,4 +14,16 @@ public class PulsePropagationMust
         var sut = new PulsePropagation(input);
         Assert.Equal(expectedCommands, sut.CommandsCount);
     }
+
+    [Theory]
+    [InlineData(SAMPLE_INPUT, 3, 1, 0)]
+    [InlineData(SECOND_SAMPLE_INPUT, 2, 2, 1)]
+    [InlineData(PUZZLE_INPUT, 48, 9, 1)]
+    public void ParseInputCorrectly(string input, int expectedFlipFlops, int expectedConjuntions, int expectedUnnameds)
+    {
+        var sut = new PulsePropagation(input);
+        Assert.Equal(expectedFlipFlops, sut.FlipFlopCount);
+        Assert.Equal(expectedConjuntions, sut.ConjuntionCount);
+        Assert.Equal(expectedUnnameds, sut.UnnamedCount);
+    }
 }
