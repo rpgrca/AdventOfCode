cb3107a9353a134bce1ecd7b250f24522c1043ed
Roberto <rpgrca@gmail.com>
2023-12-20 10:43:37 -0300

    add test, load input correctly

--- 2023/Day20/Day20.Logic/PulsePropagation.cs
+++ 2023/Day20/Day20.Logic/PulsePropagation.cs
@@ -1,6 +1,16 @@
+
 namespace Day20.Logic;
 
 public class PulsePropagation
 {
+    private readonly string _input;
+    private readonly string[] _lines;
 
+    public PulsePropagation(string input)
+    {
+        _input = input;
+        _lines = _input.Split("\n");
+    }
+
+    public int CommandsCount => _lines.Length;
 }
--- 2023/Day20/Day20.UnitTests/PulsePropagationMust.cs
+++ 2023/Day20/Day20.UnitTests/PulsePropagationMust.cs
@@ -5,9 +5,13 @@ namespace Day20.UnitTests;
 
 public class PulsePropagationMust
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData(SAMPLE_INPUT, 5)]
+    [InlineData(SECOND_SAMPLE_INPUT, 5)]
+    [InlineData(PUZZLE_INPUT, 58)]
+    public void LoadInputCorrectly(string input, int expectedCommands)
     {
-
+        var sut = new PulsePropagation(input);
+        Assert.Equal(expectedCommands, sut.CommandsCount);
     }
 }
