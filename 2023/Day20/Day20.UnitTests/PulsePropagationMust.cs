3b09a6f9359dd92e884ec5730ec5241dac1d12ba
Roberto <rpgrca@gmail.com>
2023-12-20 10:40:49 -0300

    initial setup day 20

---
+++ 2023/Day20/Day20.UnitTests/PulsePropagationMust.cs
@@ -0,0 +1,13 @@
+using Day20.Logic;
+using static Day20.UnitTests.Constants;
+
+namespace Day20.UnitTests;
+
+public class PulsePropagationMust
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
