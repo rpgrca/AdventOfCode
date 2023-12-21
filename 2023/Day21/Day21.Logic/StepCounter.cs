6e148be3476456e61e691f6895ec2e0a8d586127
Roberto <rpgrca@gmail.com>
2023-12-21 10:55:49 -0300

    add test, execute one step correctly

--- 2023/Day21/Day21.Logic/StepCounter.cs
+++ 2023/Day21/Day21.Logic/StepCounter.cs
@@ -1,4 +1,9 @@
 
+
+
+
+using System.Runtime.CompilerServices;
+
 namespace Day21.Logic;
 
 public class StepCounter
@@ -6,11 +11,89 @@ public class StepCounter
     private readonly string _input;
     private readonly string[] _lines;
 
-    public int Width => _lines.Length;
+    public int Height => _lines.Length;
+    public int Width => _lines[0].Length;
+
+    public int StartingX { get; }
+    public int StartingY { get; }
+    public List<(int X, int Y)> CurrentPositions { get; private set; }
 
     public StepCounter(string input)
     {
         _input = input;
         _lines = _input.Split("\n");
+
+        for (var y = 0; y < Height; y++)
+        {
+            for (var x = 0; x < Width; x++)
+            {
+                if (_lines[y][x] == 'S')
+                {
+                    StartingX = x;
+                    StartingY = y;
+                    goto StartLocationFound;
+                }
+            }
+        }
+
+        StartLocationFound:;
+        CurrentPositions = new List<(int X, int Y)> { (StartingX, StartingY) };
+    }
+
+    public void Step()
+    {
+        var newPositions = new List<(int X, int Y)>();
+        foreach (var position in CurrentPositions)
+        {
+            if (position.X - 1 >= 0)
+            {
+                if (_lines[position.Y][position.X - 1] == '.')
+                {
+                    var newPosition = (position.X - 1, position.Y);
+                    if (! newPositions.Contains(newPosition))
+                    {
+                        newPositions.Add(newPosition);
+                    }
+                }
+            }
+
+            if (position.X + 1 < Width)
+            {
+                if (_lines[position.Y][position.X + 1] == '.')
+                {
+                    var newPosition = (position.X + 1, position.Y);
+                    if (! newPositions.Contains(newPosition))
+                    {
+                        newPositions.Add(newPosition);
+                    }
+                }
+            }
+
+            if (position.Y - 1 >= 0)
+            {
+                if (_lines[position.Y - 1][position.X] == '.')
+                {
+                    var newPosition = (position.X, position.Y - 1);
+                    if (! newPositions.Contains(newPosition))
+                    {
+                        newPositions.Add(newPosition);
+                    }
+                }
+            }
+
+            if (position.Y + 1 < Height)
+            {
+                if (_lines[position.Y + 1][position.X] == '.')
+                {
+                    var newPosition = (position.X, position.Y + 1);
+                    if (! newPositions.Contains(newPosition))
+                    {
+                        newPositions.Add(newPosition);
+                    }
+                }
+            }
+        }
+
+        CurrentPositions = newPositions;
     }
 }
--- 2023/Day21/Day21.UnitTests/StepCounterMust.cs
+++ 2023/Day21/Day21.UnitTests/StepCounterMust.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography;
 using Day21.Logic;
 using static Day21.UnitTests.Constants;
 
@@ -6,11 +7,32 @@ namespace Day21.UnitTests;
 public class StepCounterMust
 {
     [Theory]
-    [InlineData(SAMPLE_INPUT, 11)]
-    [InlineData(PUZZLE_INPUT, 131)]
-    public void LoadInputCorrectly(string input, int expectedWidth)
+    [InlineData(SAMPLE_INPUT, 11, 11)]
+    [InlineData(PUZZLE_INPUT, 131, 131)]
+    public void LoadInputCorrectly(string input, int expectedWidth, int expectedHeight)
     {
         var sut = new StepCounter(input);
         Assert.Equal(expectedWidth, sut.Width);
+        Assert.Equal(expectedHeight, sut.Height);
+    }
+
+    [Theory]
+    [InlineData(SAMPLE_INPUT, 5, 5)]
+    [InlineData(PUZZLE_INPUT, 65, 65)]
+    public void ParseInputCorrectly(string input, int expectedX, int expectedY)
+    {
+        var sut = new StepCounter(input);
+        Assert.Equal(expectedX, sut.StartingX);
+        Assert.Equal(expectedY, sut.StartingY);
+    }
+
+    [Fact]
+    public void ExecuteOneStepCorrectly()
+    {
+        var sut = new StepCounter(SAMPLE_INPUT);
+        sut.Step();
+        Assert.Collection(sut.CurrentPositions,
+            c1 => Assert.Equal((4, 5), c1),
+            c2 => Assert.Equal((5, 4), c2));
     }
 }
