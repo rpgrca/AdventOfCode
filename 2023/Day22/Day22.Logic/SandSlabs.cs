db005e98d2d69a27a5e4003ab9bbfb209d7bd139
Roberto <rpgrca@gmail.com>
2023-12-22 11:18:29 -0300

    add test, drop piece correctly when using y coordinate

--- 2023/Day22/Day22.Logic/SandSlabs.cs
+++ 2023/Day22/Day22.Logic/SandSlabs.cs
@@ -1,24 +1,34 @@
+
+using System.IO.Compression;
+
 namespace Day22.Logic;
 
 public class Brick
 {
-    public (int X, int Y, int Z) Start { get; init; }
-    public (int X, int Y, int Z) End { get; init; }
+    public (int X, int Y, int Z) Start { get; }
+    public (int X, int Y, int Z) End { get; }
+
+    public Brick((int, int, int) start, (int, int, int) end)
+    {
+        Start = start;
+        End = end;
+    }
 }
 
 public class SandSlabs
 {
     private readonly string _input;
     private readonly string[] _lines;
-    public int BrickCount => _lines.Length;
+    private List<Brick> _bricks;
+    public int BrickCount => _bricks.Count;
 
-    public List<Brick> Bricks { get; set; }
+    public List<Brick> Bricks => _bricks;
 
     public SandSlabs(string input)
     {
         _input = input;
         _lines = _input.Split("\n");
-        Bricks = new List<Brick>();
+        _bricks = new List<Brick>();
 
         foreach (var line in _lines)
         {
@@ -27,7 +37,62 @@ public class SandSlabs
                 .Select(d => (d[0], d[1], d[2]))
                 .ToArray();
 
-            Bricks.Add(new Brick { Start = points[0], End = points[1] });
+            _bricks.Add(new Brick(points[0], points[1]));
         }
+    }
+
+    public void Drop()
+    {
+        var bricksAfterDrop = new List<Brick>();
+        foreach (var brick in _bricks)
+        {
+            if (brick.Start.Z == 1 || brick.End.Z == 1)
+            {
+                bricksAfterDrop.Add(brick);
+            }
+            else
+            {
+                if (brick.Start.Z == brick.End.Z)
+                {
+                    if (brick.Start.X == brick.End.X)
+                    {
+                        for (var z = brick.Start.Z - 1; z > 0; z--)
+                        {
+                            for (var y = brick.Start.Y; y <= brick.End.Y; y++)
+                            {
+                                if (bricksAfterDrop.Any(b => (b.Start.Z == z || b.End.Z == z) &&
+                                    (b.Start.X == brick.Start.X || b.End.X == brick.Start.X) &&
+                                    b.Start.Y <= y && y <= b.End.Y))
+                                {
+                                    bricksAfterDrop.Add(new Brick((brick.Start.X, brick.Start.Y, z + 1), (brick.End.X, brick.End.Y, z + 1)));
+                                    goto Placed;
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        for (var z = brick.Start.Z - 1; z > 0; z--)
+                        {
+                            for (var x = brick.Start.X; x <= brick.End.X; x++)
+                            {
+                                if (bricksAfterDrop.Any(b => (b.Start.Z == z || b.End.Z == z) &&
+                                    (b.Start.Y == brick.Start.Y || b.End.Y == brick.Start.Y) &&
+                                    b.Start.X <= x && x <= b.End.X))
+                                {
+                                    bricksAfterDrop.Add(new Brick((brick.Start.X, brick.Start.Y, z + 1), (brick.End.X, brick.End.Y, z + 1)));
+                                    goto Placed;
+                                }
+                            }
+                        }
+
+                    }
+                }
+            }
+
+            Placed:;
+        }
+
+        _bricks = bricksAfterDrop;
     }
 }
--- 2023/Day22/Day22.UnitTests/SandSlabsMust.cs
+++ 2023/Day22/Day22.UnitTests/SandSlabsMust.cs
@@ -20,8 +20,44 @@ public class SandSlabsMust
         var sut = new SandSlabs("1,0,1~1,2,1");
         Assert.Collection(sut.Bricks,
             b1 => {
-                Assert.Equal((1,0,1), b1.Start);
-                Assert.Equal((1,2,1), b1.End);
+                Assert.Equal((1, 0, 1), b1.Start);
+                Assert.Equal((1, 2, 1), b1.End);
+            });
+    }
+
+    [Fact]
+    public void DoNotDropBricks_WhenBricksAreRestingOnEachOther()
+    {
+        var sut = new SandSlabs("1,0,1~1,2,1\n0,0,2~2,0,2");
+        sut.Drop();
+        Assert.Collection(sut.Bricks,
+            b1 => {
+                Assert.Equal((1, 0, 1), b1.Start);
+                Assert.Equal((1, 2, 1), b1.End);
+            },
+            b2 => {
+                Assert.Equal((0, 0, 2), b2.Start);
+                Assert.Equal((2, 0, 2), b2.End);
+            });
+    }
+
+    [Fact]
+    public void DropBricksCorrectly_WhenBricksHaveGapBelow()
+    {
+        var sut = new SandSlabs("1,0,1~1,2,1\n0,0,2~2,0,2\n0,2,3~2,2,3");
+        sut.Drop();
+        Assert.Collection(sut.Bricks,
+              b1 => {
+                Assert.Equal((1, 0, 1), b1.Start);
+                Assert.Equal((1, 2, 1), b1.End);
+            },
+            b2 => {
+                Assert.Equal((0, 0, 2), b2.Start);
+                Assert.Equal((2, 0, 2), b2.End);
+            },
+            b3 => {
+                Assert.Equal((0, 2, 2), b3.Start);
+                Assert.Equal((2, 2, 2), b3.End);
             });
     }
 }
