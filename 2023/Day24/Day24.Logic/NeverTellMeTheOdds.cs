483bf1e29c5dbdb22cb51a2b9acd1fdb5a131907
Roberto <rpgrca@gmail.com>
2023-12-24 10:22:48 -0300

    add test, parse input correctly

--- 2023/Day24/Day24.Logic/NeverTellMeTheOdds.cs
+++ 2023/Day24/Day24.Logic/NeverTellMeTheOdds.cs
@@ -1,5 +1,27 @@
 
+
 namespace Day24.Logic;
+
+public class Hail
+{
+    public ulong X { get; }
+    public ulong Y { get; }
+    public ulong Z { get; }
+    public int Vx { get; }
+    public int Vy { get; }
+    public int Vz { get; }
+    public ((ulong, ulong, ulong), (int, int, int)) Data => ((X, Y, Z), (Vx, Vy, Vz));
+
+    public Hail(ulong x, ulong y, ulong z, int vx, int vy, int vz)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+        Vx = vx;
+        Vy = vy;
+        Vz = vz;
+    }
+}
 
 public class NeverTellMeTheOdds
 {
@@ -8,10 +30,22 @@ public class NeverTellMeTheOdds
 
     public int HailCount => _lines.Length;
 
+    public List<Hail> Hails { get; }
+
     public NeverTellMeTheOdds(string input)
     {
         _input = input;
         _lines = _input.Split("\n");
+        Hails = new List<Hail>();
+
+        foreach (var line in _lines)
+        {
+            var values = line.Split(" @ ");
+            var positions = values[0].Split(",");
+            var velocities = values[1].Split(",");
+            Hails.Add(new Hail(ulong.Parse(positions[0]), ulong.Parse(positions[1]), ulong.Parse(positions[2]),
+                int.Parse(velocities[0]), int.Parse(velocities[1]), int.Parse(velocities[2])));
+        }
     }
 
 }
--- 2023/Day24/Day24.UnitTests/NeverTellMeTheOddsMust.cs
+++ 2023/Day24/Day24.UnitTests/NeverTellMeTheOddsMust.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography;
 using Day24.Logic;
 using static Day24.UnitTests.Constants;
 
@@ -5,9 +6,24 @@ namespace Day24.UnitTests;
 
 public class NeverTellMeTheOddsMust
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData(SAMPLE_INPUT, 5)]
+    [InlineData(PUZZLE_INPUT, 300)]
+    public void LoadInputCorrectly(string input, int expectedLength)
     {
+        var sut = new NeverTellMeTheOdds(input);
+        Assert.Equal(expectedLength, sut.HailCount);
+    }
 
+    [Fact]
+    public void ParseInputCorrectly()
+    {
+        var sut = new NeverTellMeTheOdds(SAMPLE_INPUT);
+        Assert.Collection(sut.Hails,
+            h1 => Assert.Equal(((19UL, 13UL, 30UL), (-2,  1, -2)), h1.Data),
+            h2 => Assert.Equal(((18UL, 19UL, 22UL), (-1, -1, -2)), h2.Data),
+            h3 => Assert.Equal(((20UL, 25UL, 34UL), (-2, -2, -4)), h3.Data),
+            h4 => Assert.Equal(((12UL, 31UL, 28UL), (-1, -2, -1)), h4.Data),
+            h5 => Assert.Equal(((20UL, 19UL, 15UL), ( 1, -5, -3)), h5.Data));
     }
 }
