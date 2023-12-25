8c9db7f64ef90b496898774e003d495ba6c1fd8e
Roberto <rpgrca@gmail.com>
2023-12-25 11:27:36 -0300

    add test, parse input correctly

--- 2023/Day25/Day25.Logic/Snowverload.cs
+++ 2023/Day25/Day25.Logic/Snowverload.cs
@@ -1,15 +1,41 @@
 
+
 namespace Day25.Logic;
 
 public class Snowverload
 {
     private readonly string _input;
     private readonly string[] _lines;
-    public int ComponentCount => _lines.Length;
+    public int InputLength => _lines.Length;
+
+    public Dictionary<string, List<string>> Components { get; }
 
     public Snowverload(string input)
     {
         _input = input;
         _lines = _input.Split("\n");
+
+        Components = new Dictionary<string, List<string>>();
+        foreach (var line in _lines)
+        {
+            var values = line.Split(": ");
+            var components = values[1].Split(" ");
+            foreach (var component in components)
+            {
+                if (! Components.ContainsKey(component))
+                {
+                    Components.Add(component, new List<string>());
+                }
+
+                Components[component].Add(values[0]);
+            }
+
+            if (! Components.ContainsKey(values[0]))
+            {
+                Components.Add(values[0], new List<string>());
+            }
+
+            Components[values[0]].AddRange(components);
+        }
     }
 }
--- 2023/Day25/Day25.UnitTests/SnowverloadMust.cs
+++ 2023/Day25/Day25.UnitTests/SnowverloadMust.cs
@@ -11,6 +11,15 @@ public class SnowverloadMust
     public void LoadInputCorrectly(string input, int expectedLength)
     {
         var sut = new Snowverload(input);
-        Assert.Equal(expectedLength, sut.ComponentCount);
+        Assert.Equal(expectedLength, sut.InputLength);
+    }
+
+    [Theory]
+    [InlineData(SAMPLE_INPUT, 15)]
+    [InlineData(PUZZLE_INPUT, 1552)]
+    public void ParseInputCorrectly(string input, int expectedComponentCount)
+    {
+        var sut = new Snowverload(input);
+        Assert.Equal(expectedComponentCount, sut.Components.Count);
     }
 }
