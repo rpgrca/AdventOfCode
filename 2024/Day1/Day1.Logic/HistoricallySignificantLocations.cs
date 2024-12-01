f21f8ac3dc3077f6fb7d9f893973e531d3e85a9a
Roberto <rpgrca@gmail.com>
2024-12-01 02:40:44 -0300

    solve first sample correctly

--- 2024/Day1/Day1.Logic/HistoricallySignificantLocations.cs
+++ 2024/Day1/Day1.Logic/HistoricallySignificantLocations.cs
@@ -5,22 +5,41 @@ namespace Day1.Logic;
 
 public class HistoricallySignificantLocations
 {
-    private string[] _input;
+    private readonly string[] _input;
+    private readonly List<int> _firstColumn;
+    private readonly List<int> _secondColumn;
 
     public HistoricallySignificantLocations(string input)
     {
+        _firstColumn = new();
+        _secondColumn = new();
         _input = input.Split("\n");
         Length = _input.Length;
 
+        SplitLists();
         CalculateTotalDistance();
+    }
+
+    private void SplitLists()
+    {
+        var firstColumn = new List<int>();
+        var secondColumn = new List<int>();
+        foreach (var input in _input)
+        {
+            var values = input.Split(' ').Where(p => !string.IsNullOrWhiteSpace(p)).Select(int.Parse).ToList();
+            firstColumn.Add(values[0]);
+            secondColumn.Add(values[1]);
+        }
+
+        _firstColumn.AddRange(firstColumn.Order());
+        _secondColumn.AddRange(secondColumn.Order());
     }
 
     private void CalculateTotalDistance()
     {
-        foreach (var input in _input)
+        for (var index = 0; index < Length; index++)
         {
-            var values = _input[0].Split(' ').Where(p => !string.IsNullOrWhiteSpace(p)).Select(int.Parse).ToList();
-            TotalDistance += Math.Abs(values[0] - values[1]);
+            TotalDistance += Math.Abs(_firstColumn[index] - _secondColumn[index]);
         }
     }
 
--- 2024/Day1/Day1.UnitTests/HistoricallySignificantLocationsMust.cs
+++ 2024/Day1/Day1.UnitTests/HistoricallySignificantLocationsMust.cs
@@ -18,10 +18,17 @@ public class HistoricallySignificantLocationsMust
     [InlineData("1   1", 0)]
     [InlineData("1   2", 1)]
     [InlineData("2   1", 1)]
-    [InlineData("1   2\n2   1", 2)]
+    [InlineData("1   2\n2   1", 0)]
     public void CalculateTotalDistanceCorrectly(string input, int expectedDistance)
     {
         var sut = new HistoricallySignificantLocations(input);
         Assert.Equal(expectedDistance, sut.TotalDistance);
+    }
+
+    [Fact]
+    public void SolveFirstSampleCorrectly()
+    {
+        var sut = new HistoricallySignificantLocations(SAMPLE_INPUT);
+        Assert.Equal(11, sut.TotalDistance);
     }
 }
