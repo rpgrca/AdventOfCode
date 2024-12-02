10618a220ca7808cf3967d19b4d473bbacc63b2e
Roberto <rpgrca@gmail.com>
2024-12-02 09:39:45 -0300

    add test, determine safe report

--- 2024/Day2/Day2.Logic/Reports.cs
+++ 2024/Day2/Day2.Logic/Reports.cs
@@ -1,4 +1,5 @@
 
+
 namespace Day2.Logic;
 
 public class Reports
@@ -9,7 +10,47 @@ public class Reports
     {
         _input = input.Split("\n");
         Length = _input.Length;
+
+        CountSafeReports();
+    }
+
+    private void CountSafeReports()
+    {
+        var levels = _input[0].Split(" ").Select(int.Parse).ToArray();
+
+        if (levels[0] == levels[1])
+        {
+            return;
+        }
+
+        var ascending = levels[0] < levels[1];
+        var safe = true;
+        for (var index = 0; index < levels.Length - 1; index++)
+        {
+            var difference = levels[index + 1] - levels[index];
+            if (difference == 0)
+            {
+                break;
+            }
+
+            if ((ascending && (levels[index + 1] <= levels[index])) || (!ascending && (levels[index + 1] >= levels[index])))
+            {
+                break;
+            }
+
+            var absolute = Math.Abs(difference);
+            if (absolute < 1 || absolute > 3)
+            {
+                safe = false;
+            }
+        }
+
+        if (safe)
+        {
+            SafeReportsCount += 1;
+        }
     }
 
     public int Length { get; private set; }
+    public int SafeReportsCount { get; private set; }
 }
--- 2024/Day2/Day2.UnitTests/ReportsMust.cs
+++ 2024/Day2/Day2.UnitTests/ReportsMust.cs
@@ -13,4 +13,13 @@ public class ReportsMust
         var sut = new Reports(input);
         Assert.Equal(expectedLength, sut.Length);
     }
+
+    [Theory]
+    [InlineData("7 6 4 2 1", 1)]
+    [InlineData("1 2 7 8 9", 0)]
+    public void DetermineSafeReportCorrectly(string input, int expectedCount)
+    {
+        var sut = new Reports(input);
+        Assert.Equal(expectedCount, sut.SafeReportsCount);
+    }
 }
