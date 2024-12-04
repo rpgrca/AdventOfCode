63b2446e3e0c6d32724b43d104a16ad4c88fee0a
Roberto <rpgrca@gmail.com>
2024-12-04 10:18:51 -0300

    initial setup day 4

---
+++ 2024/Day4/Day4.Logic/WordSearch.cs
@@ -0,0 +1,7 @@
+namespace Day4.Logic;
+
+public class WordSearch
+{
+
+
+}
---
+++ 2024/Day4/Day4.UnitTests/WordSearchMust.cs
@@ -0,0 +1,13 @@
+using Day4.Logic;
+using static Day4.UnitTests.Constants;
+
+namespace Day4.UnitTests;
+
+public class WorldSearchMust
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
