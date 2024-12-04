31ccb7cc625f1bc40c038ba06e93668dc5bf330d
Roberto <rpgrca@gmail.com>
2024-12-04 10:37:21 -0300

    add test, find xmas horizontally

--- 2024/Day4/Day4.Logic/WordSearch.cs
+++ 2024/Day4/Day4.Logic/WordSearch.cs
@@ -1,4 +1,7 @@
 
+
+using System.Linq.Expressions;
+
 namespace Day4.Logic;
 
 public class WordSearch
@@ -10,7 +13,48 @@ public class WordSearch
     {
         _input = input;
         _block = input.Split("\n");
+
+        FindXmas();
+    }
+
+    private void FindXmas()
+    {
+        for (var y = 0; y < Length; y++)
+        {
+            for (var x = 0; x < Length; x++)
+            {
+                if (_block[y][x] == 'X')
+                {
+                    FindXmasHorizontally(y, x);
+                    FindXmasReverseHorizontally(y, x);
+                }
+            }
+        }
+    }
+
+    private void FindXmasHorizontally(int y, int x)
+    {
+        if (x <= Length - 4)
+        {
+            if (_block[y][x+1] == 'M' && _block[y][x+2] == 'A' && _block[y][x+3] == 'S')
+            {
+                XmasCount++;
+            }
+        }
+    }
+
+    private void FindXmasReverseHorizontally(int y, int x)
+    {
+        if (x >= 3)
+        {
+            if (_block[y][x-1] == 'M' && _block[y][x-2] == 'A' && _block[y][x-3] == 'S')
+            {
+                XmasCount++;
+            }
+        }
     }
 
     public int Length => _block.Length;
+
+    public int XmasCount { get; private set; }
 }
--- 2024/Day4/Day4.UnitTests/WordSearchMust.cs
+++ 2024/Day4/Day4.UnitTests/WordSearchMust.cs
@@ -7,10 +7,33 @@ public class UnitTest1
 {
     [Theory]
     [InlineData(SAMPLE_INPUT, 10)]
+    [InlineData(SECOND_SAMPLE_INPUT, 5)]
     [InlineData(PUZZLE_INPUT, 140)]
     public void LoadInputCorrectly(string input, int expectedLength)
     {
         var sut = new WordSearch(input);
         Assert.Equal(expectedLength, sut.Length);
+    }
+
+    [Theory]
+    [InlineData(@"......
+......
+......
+XMAS..
+......", 1)]
+    [InlineData(@"......
+.SAMX.
+......
+......
+.....", 1)]
+    [InlineData(@"......
+.SAMX.
+......
+XMAS..
+......", 2)]
+    public void FindXmasHorizontally(string input, int expectedLength)
+    {
+        var sut = new WordSearch(input);
+        Assert.Equal(expectedLength, sut.XmasCount);
     }
 }
