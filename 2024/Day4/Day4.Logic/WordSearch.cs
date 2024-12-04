4d65b181df044a4186cbe5136c9740128ce6a1e9
Roberto <rpgrca@gmail.com>
2024-12-04 10:21:39 -0300

    add test, load input correctly

--- 2024/Day4/Day4.Logic/WordSearch.cs
+++ 2024/Day4/Day4.Logic/WordSearch.cs
@@ -1,7 +1,16 @@
+
 namespace Day4.Logic;
 
 public class WordSearch
 {
+    private readonly string _input;
+    private readonly string[] _block;
 
+    public WordSearch(string input)
+    {
+        _input = input;
+        _block = input.Split("\n");
+    }
 
+    public int Length => _block.Length;
 }
--- 2024/Day4/Day4.UnitTests/WordSearchMust.cs
+++ 2024/Day4/Day4.UnitTests/WordSearchMust.cs
@@ -3,11 +3,14 @@ using static Day4.UnitTests.Constants;
 
 namespace Day4.UnitTests;
 
-public class WorldSearchMust
+public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData(SAMPLE_INPUT, 10)]
+    [InlineData(PUZZLE_INPUT, 140)]
+    public void LoadInputCorrectly(string input, int expectedLength)
     {
-
+        var sut = new WordSearch(input);
+        Assert.Equal(expectedLength, sut.Length);
     }
 }
