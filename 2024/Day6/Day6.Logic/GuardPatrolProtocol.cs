766a35e8b9ed0c4631f503b62d3b52677e988513
Roberto <rpgrca@gmail.com>
2024-12-06 10:49:25 -0300

    add test, discover possible obstacle position correctly

--- 2024/Day6/Day6.Logic/GuardPatrolProtocol.cs
+++ 2024/Day6/Day6.Logic/GuardPatrolProtocol.cs
@@ -1,16 +1,29 @@
 
 
+
 namespace Day6.Logic;
 
 public class GuardPatrolProtocol
 {
     private readonly string _input;
     private readonly char[][] _map;
+    private readonly List<List<HashSet<int>>> _cache;
+    private readonly List<(int X, int Y, int Direction)> _steps;
 
     public GuardPatrolProtocol(string input)
     {
         _input = input;
+        _steps = new();
         _map = _input.Split('\n').Select(p => p.ToCharArray()).ToArray();
+        _cache = new();
+        for (var y = 0; y < Length; y++)
+        {
+            _cache.Add(new List<HashSet<int>>());
+            for (var x = 0; x < Length; x++)
+            {
+                _cache[y].Add(new HashSet<int>());
+            }
+        }
 
         TransverseMap();
     }
@@ -44,15 +57,27 @@ found:
         var currentX = initialX;
         var currentY = initialY;
         var nextX = currentX + directions[currentDirection % 4].X;
-        var nextY = currentY + directions[currentDirection % 4].Y;
-
+        var nextY = currentY + directions[currentDirectSystem.ArgumentOutOfRangeException
         while (nextX >= 0 && nextX < Length && nextY >= 0 && nextY < Length)
         {
             if (_map[nextY][nextX] != '#')
             {
+                var possibleDirection = (currentDirection + 1) % 4;
+                var obstacleChangedDirection = directions[possibleDirection];
+                var possibleX = currentX + obstacleChangedDirection.X;
+                var possibleY = currentY + obstacleChangedDirection.Y;
+                if (possibleX >= 0 && possibleX < Length && possibleY >= 0 && possibleY < Length)
+                {
+                    if (_cache[possibleY][possibleX].Contains(possibleDirection))
+                    {
+                        PossibleObstructions++;
+                    }
+                }
+
                 currentX = nextX;
                 currentY = nextY;
                 _map[currentY][currentX] = 'X';
+                _cache[currentY][currentX].Add(currentDirection);
             }
             else
             {
@@ -77,6 +102,6 @@ found:
     }
 
     public int Length => _map.Length;
-
     public int DistinctVisitedPositions { get; private set; }
+    public int PossibleObstructions { get; private set; }
 }
