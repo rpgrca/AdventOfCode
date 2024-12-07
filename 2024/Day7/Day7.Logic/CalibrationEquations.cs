ba24609435e77e22d0e8c7595ee3d12ebf9a9a5b
Roberto <rpgrca@gmail.com>
2024-12-07 11:26:22 -0300

    add test, calculate combinations correctly

--- 2024/Day7/Day7.Logic/CalibrationEquations.cs
+++ 2024/Day7/Day7.Logic/CalibrationEquations.cs
@@ -1,3 +1,4 @@
+
 namespace Day7.Logic;
 
 public class CalibrationEquations
@@ -7,9 +8,68 @@ public class CalibrationEquations
 
     public int Count => _equations.Length;
 
+    public long TotalCalibration { get; private set; }
+
     public CalibrationEquations(string input)
     {
         _input = input;
         _equations = _input.Split('\n');
+
+        Parse();
+    }
+
+    private void Parse()
+    {
+        foreach (var equation in _equations)
+        {
+            var members = equation.Split(':', StringSplitOptions.TrimEntries);
+            var result = long.Parse(members[0]);
+            var factors = members[1]
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(long.Parse)
+                .ToArray();
+
+            var combinations = new List<string>();
+            Combine(combinations, factors.Length - 1, "");
+
+            if (CanSolveEquation(result, factors, combinations))
+            {
+                TotalCalibration = result;
+            }
+        }
+    }
+
+    private bool CanSolveEquation(long result, long[] factors, List<string> combinations)
+    {
+        foreach (var combination in combinations)
+        {
+            var solution = factors[0];
+            for (var index = 0; index < factors.Length - 1; index++)
+            {
+                solution = combination[index] switch {
+                    '*' => solution * factors[index + 1],
+                    '+' => solution + factors[index + 1]
+                };
+            }
+
+            if (solution == result)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void Combine(List<string> combinations, int count, string equation)
+    {
+        if (count == 0)
+        {
+            combinations.Add(equation);
+            return;
+        }
+
+        Combine(combinations, count - 1, equation + "+");
+        Combine(combinations, count - 1, equation + "*");
     }
 }
--- 2024/Day7/Day7.UnitTests/CalibrationEquationsMust.cs
+++ 2024/Day7/Day7.UnitTests/CalibrationEquationsMust.cs
@@ -13,4 +13,13 @@ public class CalibrationEquationsMust
         var sut = new CalibrationEquations(input);
         Assert.Equal(expectedCount, sut.Count);
     }
+
+    [Theory]
+    [InlineData("190: 10 19", 190)]
+    [InlineData("83: 17 5", 0)]
+    public void ReturnCorrectTotalCalibration(string input, int expectedResult)
+    {
+        var sut = new CalibrationEquations(input);
+        Assert.Equal(expectedResult, sut.TotalCalibration);
+    }
 }
