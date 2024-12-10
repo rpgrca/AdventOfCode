79da1ae334dd692bb4a0fafcdb772c6c80ba9c8c
Roberto <rpgrca@gmail.com>
2024-12-10 09:47:22 -0300

    add test, locate trailheads correctly

--- 2024/Day10/Day10.Logic/TopographicMap.cs
+++ 2024/Day10/Day10.Logic/TopographicMap.cs
@@ -1,4 +1,5 @@
 
+
 namespace Day10.Logic;
 
 public class TopographicMap
@@ -6,11 +7,29 @@ public class TopographicMap
     private readonly string _input;
     private readonly string[] _lines;
 
+    public int Size => _lines.Length;
+    public List<(int X, int Y)> Trailheads { get; private set; }
+
     public TopographicMap(string input)
     {
         _input = input;
         _lines = input.Split('\n');
+        Trailheads = new();
+
+        Parse();
     }
 
-    public int Size => _lines.Length;
+    private void Parse()
+    {
+        for (var y = 0; y < Size; y++)
+        {
+            for (var x = 0; x < Size; x++)
+            {
+                if (_lines[y][x] == '0')
+                {
+                    Trailheads.Add((x, y));
+                }
+            }
+        }
+    }
 }
--- 2024/Day10/Day10.UnitTests/TopographicMapMust.cs
+++ 2024/Day10/Day10.UnitTests/TopographicMapMust.cs
@@ -1,4 +1,7 @@
+using System.Numerics;
+using System.Security.Cryptography;
 using Day10.Logic;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Host;
 using static Day10.UnitTests.Constants;
 
 namespace Day10.UnitTests;
@@ -15,5 +18,46 @@ public class TopographicMapMust
     {
         var sut = new TopographicMap(input);
         Assert.Equal(expectedSize, sut.Size);
+    }
+
+    [Fact]
+    public void LocateTrailheadsCorrectly_WhenUsingSecondSample()
+    {
+        var sut = new TopographicMap(SECOND_SAMPLE_INPUT);
+        Assert.Collection(sut.Trailheads, p1 => Assert.Equal((0, 0), p1));
+    }
+
+    [Theory]
+    [InlineData(THIRD_SAMPLE_INPUT)]
+    [InlineData(FOURTH_SAMPLE_INPUT)]
+    public void LocateTrailheadsCorrectly_WhenUsingThirdOrFourthSample(string input)
+    {
+        var sut = new TopographicMap(input);
+        Assert.Collection(sut.Trailheads, p1 => Assert.Equal((3, 0), p1));
+    }
+
+    [Fact]
+    public void LocateTrailheadsCorrectly_WhenUsingFifthSample()
+    {
+        var sut = new TopographicMap(FIFTH_SAMPLE_INPUT);
+        Assert.Collection(sut.Trailheads,
+            p1 => Assert.Equal((1, 0), p1),
+            p2 => Assert.Equal((5, 6), p2));
+    }
+
+    [Fact]
+    public void LocateTrailheadsCorrectly_WhenUsingSampleMap()
+    {
+        var sut = new TopographicMap(SAMPLE_INPUT);
+        Assert.Collection(sut.Trailheads,
+            p1 => Assert.Equal((2, 0), p1),
+            p2 => Assert.Equal((4, 0), p2),
+            p3 => Assert.Equal((4, 2), p3),
+            p4 => Assert.Equal((6, 4), p4),
+            p5 => Assert.Equal((2, 5), p5),
+            p6 => Assert.Equal((5, 5), p6),
+            p7 => Assert.Equal((0, 6), p7),
+            p8 => Assert.Equal((6, 6), p8),
+            p9 => Assert.Equal((1, 7), p9));
     }
 }
