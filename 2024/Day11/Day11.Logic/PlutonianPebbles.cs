c94baa4f463628784b2a0922fc502af6f3f92dfc
Roberto <rpgrca@gmail.com>
2024-12-11 11:09:14 -0300

    add test, load input correctly

--- 2024/Day11/Day11.Logic/PlutonianPebbles.cs
+++ 2024/Day11/Day11.Logic/PlutonianPebbles.cs
@@ -3,13 +3,13 @@ namespace Day11.Logic;
 public class PlutonianPebbles
 {
     private readonly string _input;
+    private List<long> _line;
 
-    public int Count { get; private set;} = 2;
+    public int Count => _line.Count;
 
     public PlutonianPebbles(string input)
     {
         _input = input;
+        _line = _input.Split(' ').Select(long.Parse).ToList();
     }
-
-
 }
--- 2024/Day11/Day11.UnitTests/PlutonianPebblesMust.cs
+++ 2024/Day11/Day11.UnitTests/PlutonianPebblesMust.cs
@@ -7,6 +7,8 @@ public class PlutonianPebblesMust
 {
     [Theory]
     [InlineData(SAMPLE_INPUT, 2)]
+    [InlineData(SECOND_SAMPLE_INPUT, 5)]
+    [InlineData(PUZZLE_INPUT, 8)]
     public void LoadInputCorrectly(string input, int expectedCount)
     {
         var sut = new PlutonianPebbles(input);
