f04dd49a87285029c1e0f634074848b274d8a918
Roberto <rpgrca@gmail.com>
2024-12-11 12:26:51 -0300

    add test, count pebbles without creating list

--- 2024/Day11/Day11.Logic/PlutonianPebbles.cs
+++ 2024/Day11/Day11.Logic/PlutonianPebbles.cs
@@ -1,16 +1,12 @@
-
-
 namespace Day11.Logic;
 
 public class PlutonianPebbles
 {
     private readonly string _input;
     private readonly List<long> _line;
-    private readonly LinkedList<long> _pebbles;
+    private readonly Dictionary<long, long> _pebbles;
 
-    public int Count => _pebbles.Count;
-
-    public LinkedList<long> Pebbles => _pebbles;
+    public long Count => _pebbles.Values.Sum();
 
     public PlutonianPebbles(string input)
     {
@@ -19,44 +15,68 @@ public class PlutonianPebbles
         _pebbles = new();
         foreach (var number in _line)
         {
-            _pebbles.AddLast(new LinkedListNode<long>(number));
+            _pebbles.TryAdd(number, 0);
+            _pebbles[number]++;
         }
     }
 
     public void Blink(int count = 1)
     {
+        var cycle = new Dictionary<long, long>();
         for (; count > 0; count--)
         {
-            var current = _pebbles.First;
-            while (current != null)
+            var keys = _pebbles.Where(p => p.Value > 0).Select(p => p.Key).ToArray();
+            cycle.Clear();
+
+            foreach (var key in keys)
             {
-                if (current.Value == 0)
+                if (key == 0)
                 {
-                    current.Value = 1;
+                    cycle.TryAdd(1, 0);
+                    cycle[1] += _pebbles[0];
+                    cycle.TryAdd(0, 0);
+                    cycle[0] -= _pebbles[0];
                 }
                 else
                 {
-                    if (current.Value == 1)
+                    if (key == 1)
                     {
-                        current.Value *= 2024;
+                        cycle.TryAdd(2024, 0);
+                        cycle[2024] += _pebbles[1];
+                        cycle.TryAdd(1, 0);
+                        cycle[1] -= _pebbles[1];
                     }
                     else
                     {
-                        var valueAsString = current.Value.ToString();
+                        var valueAsString = key.ToString();
                         if (valueAsString.Length % 2 == 0)
                         {
-                            var left = valueAsString[0..(valueAsString.Length / 2)];
-                            var right = valueAsString[(valueAsString.Length / 2)..];
-                            _pebbles.AddBefore(current, long.Parse(left));
-                            current.Value = long.Parse(right);
+                            var left = long.Parse(valueAsString[0..(valueAsString.Length / 2)]);
+                            var right = long.Parse(valueAsString[(valueAsString.Length / 2)..]);
+                            cycle.TryAdd(left, 0);
+                            cycle[left] += _pebbles[key];
+
+                            cycle.TryAdd(right, 0);
+                            cycle[right] += _pebbles[key];
+                            cycle.TryAdd(key, 0);
+                            cycle[key] -= _pebbles[key];
                         }
                         else
                         {
-                            current.Value *= 2024;
+                            var value = key * 2024;
+                            cycle.TryAdd(value, 0);
+                            cycle[value] += _pebbles[key];
+                            cycle.TryAdd(key, 0);
+                            cycle[key] -= _pebbles[key];
                         }
                     }
                 }
-                current = current.Next;
+            }
+
+            foreach (var (key, value) in cycle)
+            {
+                _pebbles.TryAdd(key, 0);
+                _pebbles[key] += value;
             }
         }
     }
--- 2024/Day11/Day11.UnitTests/PlutonianPebblesMust.cs
+++ 2024/Day11/Day11.UnitTests/PlutonianPebblesMust.cs
@@ -1,7 +1,4 @@
-using System.Numerics;
-using System.Security.Cryptography;
 using Day11.Logic;
-using Microsoft.VisualStudio.TestPlatform.ObjectModel.Host;
 using static Day11.UnitTests.Constants;
 
 namespace Day11.UnitTests;
@@ -23,7 +20,7 @@ public class PlutonianPebblesMust
     {
         var sut = new PlutonianPebbles("0");
         sut.Blink();
-        Assert.Collection(sut.Pebbles, p1 => Assert.Equal(1, p1));
+        Assert.Equal(1, sut.Count);
     }
 
     [Fact]
@@ -31,7 +28,7 @@ public class PlutonianPebblesMust
     {
         var sut = new PlutonianPebbles("1");
         sut.Blink();
-        Assert.Collection(sut.Pebbles, p1 => Assert.Equal(2024, p1));
+        Assert.Equal(1, sut.Count);
     }
 
     [Theory]
@@ -41,9 +38,7 @@ public class PlutonianPebblesMust
     {
         var sut = new PlutonianPebbles(input);
         sut.Blink();
-        Assert.Collection(sut.Pebbles,
-            p1 => Assert.Equal(expectedLeft, p1),
-            p2 => Assert.Equal(expectedRight, p2));
+        Assert.Equal(2, sut.Count);
     }
 
     [Fact]
@@ -51,7 +46,7 @@ public class PlutonianPebblesMust
     {
         var sut = new PlutonianPebbles("999");
         sut.Blink();
-        Assert.Collection(sut.Pebbles, p1 => Assert.Equal(2021976, p1));
+        Assert.Equal(1, sut.Count);
     }
 
     [Fact]
@@ -59,14 +54,7 @@ public class PlutonianPebblesMust
     {
         var sut = new PlutonianPebbles(SECOND_SAMPLE_INPUT);
         sut.Blink();
-        Assert.Collection(sut.Pebbles,
-            p1 => Assert.Equal(1, p1),
-            p2 => Assert.Equal(2024, p2),
-            p3 => Assert.Equal(1, p3),
-            p4 => Assert.Equal(0, p4),
-            p5 => Assert.Equal(9, p5),
-            p6 => Assert.Equal(9, p6),
-            p7 => Assert.Equal(2021976, p7));
+        Assert.Equal(7, sut.Count);
     }
 
     [Fact]
@@ -74,10 +62,7 @@ public class PlutonianPebblesMust
     {
         var sut = new PlutonianPebbles(SAMPLE_INPUT);
         sut.Blink();
-        Assert.Collection(sut.Pebbles,
-            p1 => Assert.Equal(253000, p1),
-            p2 => Assert.Equal(1, p2),
-            p3 => Assert.Equal(7, p3));
+        Assert.Equal(3, sut.Count);
     }
 
     [Fact]
@@ -85,11 +70,7 @@ public class PlutonianPebblesMust
     {
         var sut = new PlutonianPebbles(SAMPLE_INPUT);
         sut.Blink(2);
-        Assert.Collection(sut.Pebbles,
-            p1 => Assert.Equal(253, p1),
-            p2 => Assert.Equal(0, p2),
-            p3 => Assert.Equal(2024, p3),
-            p4 => Assert.Equal(14168, p4));
+        Assert.Equal(4, sut.Count);
     }
 
     [Fact]
@@ -97,12 +78,7 @@ public class PlutonianPebblesMust
     {
         var sut = new PlutonianPebbles(SAMPLE_INPUT);
         sut.Blink(3);
-        Assert.Collection(sut.Pebbles,
-            p1 => Assert.Equal(512072, p1),
-            p2 => Assert.Equal(1, p2),
-            p3 => Assert.Equal(20, p3),
-            p4 => Assert.Equal(24, p4),
-            p5 => Assert.Equal(28676032, p5));
+        Assert.Equal(5, sut.Count);
     }
 
     [Fact]
@@ -110,16 +86,7 @@ public class PlutonianPebblesMust
     {
         var sut = new PlutonianPebbles(SAMPLE_INPUT);
         sut.Blink(4);
-        Assert.Collection(sut.Pebbles,
-            p1 => Assert.Equal(512, p1),
-            p2 => Assert.Equal(72, p2),
-            p3 => Assert.Equal(2024, p3),
-            p4 => Assert.Equal(2, p4),
-            p5 => Assert.Equal(0, p5),
-            p6 => Assert.Equal(2, p6),
-            p7 => Assert.Equal(4, p7),
-            p8 => Assert.Equal(2867, p8),
-            p9 => Assert.Equal(6032, p9));
+        Assert.Equal(9, sut.Count);
     }
 
     [Fact]
@@ -127,22 +94,17 @@ public class PlutonianPebblesMust
     {
         var sut = new PlutonianPebbles(SAMPLE_INPUT);
         sut.Blink(5);
-        Assert.Collection(sut.Pebbles,
-            p1 => Assert.Equal(1036288, p1),
-            p2 => Assert.Equal(7, p2),
-            p3 => Assert.Equal(2, p3),
-            p4 => Assert.Equal(20, p4),
-            p5 => Assert.Equal(24, p5),
-            p6 => Assert.Equal(4048, p6),
-            p7 => Assert.Equal(1, p7),
-            p8 => Assert.Equal(4048, p8),
-            p9 => Assert.Equal(8096, p9),
-            p10 => Assert.Equal(28, p10),
-            p11 => Assert.Equal(67, p11),
-            p12 => Assert.Equal(60, p12),
-            p13 => Assert.Equal(32, p13));
+        Assert.Equal(13, sut.Count);
     }
 
+    [Fact]
+    public void UpdatePebblesCorrectlyOnSample_WhenBlinkingSixTimes()
+    {
+        var sut  =new PlutonianPebbles(SAMPLE_INPUT);
+        sut.Blink(6);
+        Assert.Equal(22, sut.Count);
+    }
+/*
     [Fact]
     public void SolveFirstSampleCorrectly()
     {
@@ -158,4 +120,12 @@ public class PlutonianPebblesMust
         sut.Blink(25);
         Assert.Equal(189092, sut.Count);
     }
+/*
+    [Fact]
+    public void SolveSecondPuzzleCorrectly()
+    {
+        var sut = new PlutonianPebbles(PUZZLE_INPUT);
+        sut.Blink(75);
+        Assert.Equal(0, sut.Count);
+    }*/
 }
