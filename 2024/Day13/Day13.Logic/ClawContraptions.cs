963199f0d2597c0c4c8a882476f8fcaa4565518f
Roberto <rpgrca@gmail.com>
2024-12-13 10:56:14 -0300

    use cramer's rule

--- 2024/Day13/Day13.Logic/ClawContraptions.cs
+++ 2024/Day13/Day13.Logic/ClawContraptions.cs
@@ -1,24 +1,44 @@
+using System.Runtime.Intrinsics.X86;
+
 namespace Day13.Logic;
 
 public class ClawContraptions
 {
     private readonly string _input;
     private readonly ulong _adjust;
-    private readonly List<((int OffsetX, int OffsetY) ButtonA, (int OffsetX, int OffsetY) ButtonB, (ulong X, ulong Y) Prize)> _contraptions;
+    private readonly bool _adjusting;
+    private readonly List<((ulong OffsetX, ulong OffsetY) ButtonA, (ulong OffsetX, ulong OffsetY) ButtonB, (ulong X, ulong Y) Prize)> _contraptions;
 
-    public List<((int OffsetX, int OffsetY) ButtonA, (int OffsetX, int OffsetY) ButtonB, (ulong X, ulong Y) Prize)> Contraptions => _contraptions;
+    public List<((ulong OffsetX, ulong OffsetY) ButtonA, (ulong OffsetX, ulong OffsetY) ButtonB, (ulong X, ulong Y) Prize)> Contraptions => _contraptions;
     public int Count => _contraptions.Count;
 
-    public int CheapestWin { get; private set; }
+    public ulong CheapestWin { get; private set; }
 
-    public ClawContraptions(string input, ulong adjust = 0UL)
+    public ClawContraptions(string input, ulong? adjust = null)
     {
         _input = input;
-        _adjust = adjust;
         _contraptions = new();
+        if (adjust.HasValue)
+        {
+            _adjust = adjust.Value;
+            _adjusting = true;
+        }
+        else
+        {
+            _adjust = 0UL;
+            _adjusting = false;
+        }
 
         Parse();
-        FindCheapestWin();
+
+        if (_adjusting)
+        {
+            FindCheapestWin2();
+        }
+        else
+        {
+            FindCheapestWin();
+        }
     }
 
     private void Parse()
@@ -27,22 +47,22 @@ public class ClawContraptions
         foreach (var machine in machines)
         {
             var data = machine.Split('\n').Select(p => p.Split(':'));
-            (int, int) buttonA = default;
-            (int, int) buttonB = default;
+            (ulong, ulong) buttonA = default;
+            (ulong, ulong) buttonB = default;
             (ulong, ulong) prize = default;
             foreach (var dat in data)
             {
                 if (dat[0] == "Button A")
                 {
                     var info = dat[1].Split(',', StringSplitOptions.TrimEntries)
-                        .Select(p => int.Parse(p[2..]))
+                        .Select(p => ulong.Parse(p[2..]))
                         .ToList();
                     buttonA = (info[0], info[1]);
                 }
                 else if (dat[0] == "Button B")
                 {
                     var info = dat[1].Split(',', StringSplitOptions.TrimEntries)
-                        .Select(p => int.Parse(p[2..]))
+                        .Select(p => ulong.Parse(p[2..]))
                         .ToList();
                     buttonB = (info[0], info[1]);
                 }
@@ -59,17 +79,32 @@ public class ClawContraptions
         }
     }
 
+    private void FindCheapestWin2()
+    {
+        foreach (var contraption in _contraptions)
+        {
+            var determinant = (long)(contraption.ButtonA.OffsetX*contraption.ButtonB.OffsetY - contraption.ButtonB.OffsetX*contraption.ButtonA.OffsetY);
+            if (determinant != 0)
+            {
+                var a = (long)(contraption.Prize.X*contraption.ButtonB.OffsetY - contraption.ButtonB.OffsetX*contraption.Prize.Y) / determinant;
+                var b = (long)(contraption.ButtonA.OffsetX*contraption.Prize.Y - contraption.Prize.X*contraption.ButtonA.OffsetY) / determinant;
+
+                CheapestWin += (ulong)a*3 + (ulong)b;
+            }
+        }
+    }
+
     private void FindCheapestWin()
     {
         foreach (var contraption in _contraptions)
         {
-            int? cheapestWin = null;
-            for (var a = 0; a <= 100; a++)
+            ulong? cheapestWin = null;
+            for (ulong a = 0; a <= 100; a++)
             {
-                for (var b = 0; b <= 100; b++)
+                for (ulong b = 0; b <= 100; b++)
                 {
-                    if (((ulong)(contraption.ButtonA.OffsetX * a + contraption.ButtonB.OffsetX * b) == contraption.Prize.X) &&
-                        ((ulong)(contraption.ButtonA.OffsetY * a + contraption.ButtonB.OffsetY * b) == contraption.Prize.Y))
+                    if (((contraption.ButtonA.OffsetX * a + contraption.ButtonB.OffsetX * b) == contraption.Prize.X) &&
+                        ((contraption.ButtonA.OffsetY * a + contraption.ButtonB.OffsetY * b) == contraption.Prize.Y))
                     {
                         var value = a * 3 + b;
                         if (! cheapestWin.HasValue || value < cheapestWin)
--- 2024/Day13/Day13.UnitTests/ClawContraptionsMust.cs
+++ 2024/Day13/Day13.UnitTests/ClawContraptionsMust.cs
@@ -20,23 +20,23 @@ public class ClawContraptionsMust
         var sut = new ClawContraptions(SAMPLE_INPUT);
         Assert.Collection(sut.Contraptions,
             p1 => {
-                Assert.Equal((94, 34), p1.ButtonA);
-                Assert.Equal((22, 67), p1.ButtonB);
+                Assert.Equal((94UL, 34UL), p1.ButtonA);
+                Assert.Equal((22UL, 67UL), p1.ButtonB);
                 Assert.Equal((8400UL, 5400UL), p1.Prize);
             },
             p2 => {
-                Assert.Equal((26, 66), p2.ButtonA);
-                Assert.Equal((67, 21), p2.ButtonB);
+                Assert.Equal((26UL, 66UL), p2.ButtonA);
+                Assert.Equal((67UL, 21UL), p2.ButtonB);
                 Assert.Equal((12748UL, 12176UL), p2.Prize);
             },
             p3 => {
-                Assert.Equal((17, 86), p3.ButtonA);
-                Assert.Equal((84, 37), p3.ButtonB);
+                Assert.Equal((17UL, 86UL), p3.ButtonA);
+                Assert.Equal((84UL, 37UL), p3.ButtonB);
                 Assert.Equal((7870UL, 6450UL), p3.Prize);
             },
             p1 => {
-                Assert.Equal((69, 23), p1.ButtonA);
-                Assert.Equal((27, 71), p1.ButtonB);
+                Assert.Equal((69UL, 23UL), p1.ButtonA);
+                Assert.Equal((27UL, 71UL), p1.ButtonB);
                 Assert.Equal((18641UL, 10279UL), p1.Prize);
             });
     }
@@ -44,7 +44,7 @@ public class ClawContraptionsMust
     [Theory]
     [InlineData("Button A: X+94, Y+34\nButton B: X+22, Y+67\nPrize: X=8400, Y=5400", 280)]
     [InlineData("Button A: X+17, Y+86\nButton B: X+84, Y+37\nPrize: X=7870, Y=6450", 200)]
-    public void FindBestCombination(string input, int expectedWin)
+    public void FindBestCombination(string input, ulong expectedWin)
     {
         var sut = new ClawContraptions(input);
         Assert.Equal(expectedWin, sut.CheapestWin);
@@ -56,21 +56,21 @@ public class ClawContraptionsMust
     public void ReturnZero_WhenNoCombinationHasBeenFound(string input)
     {
         var sut = new ClawContraptions(input);
-        Assert.Equal(0, sut.CheapestWin);
+        Assert.Equal(0UL, sut.CheapestWin);
     }
 
     [Fact]
     public void SolveFirstSampleCorrectly()
     {
         var sut = new ClawContraptions(SAMPLE_INPUT);
-        Assert.Equal(480, sut.CheapestWin);
+        Assert.Equal(480UL, sut.CheapestWin);
     }
 
     [Fact]
     public void SolveFirstPuzzleCorrectly()
     {
         var sut = new ClawContraptions(PUZZLE_INPUT);
-        Assert.Equal(28059, sut.CheapestWin);
+        Assert.Equal(28059UL, sut.CheapestWin);
     }
 
     [Fact]
@@ -79,23 +79,23 @@ public class ClawContraptionsMust
         var sut = new ClawContraptions(SAMPLE_INPUT, 10000000000000);
         Assert.Collection(sut.Contraptions,
             p1 => {
-                Assert.Equal((94, 34), p1.ButtonA);
-                Assert.Equal((22, 67), p1.ButtonB);
+                Assert.Equal((94UL, 34UL), p1.ButtonA);
+                Assert.Equal((22UL, 67UL), p1.ButtonB);
                 Assert.Equal((10000000008400UL, 10000000005400UL), p1.Prize);
             },
             p2 => {
-                Assert.Equal((26, 66), p2.ButtonA);
-                Assert.Equal((67, 21), p2.ButtonB);
+                Assert.Equal((26UL, 66UL), p2.ButtonA);
+                Assert.Equal((67UL, 21UL), p2.ButtonB);
                 Assert.Equal((10000000012748UL, 10000000012176UL), p2.Prize);
             },
             p3 => {
-                Assert.Equal((17, 86), p3.ButtonA);
-                Assert.Equal((84, 37), p3.ButtonB);
+                Assert.Equal((17UL, 86UL), p3.ButtonA);
+                Assert.Equal((84UL, 37UL), p3.ButtonB);
                 Assert.Equal((10000000007870UL, 10000000006450UL), p3.Prize);
             },
             p1 => {
-                Assert.Equal((69, 23), p1.ButtonA);
-                Assert.Equal((27, 71), p1.ButtonB);
+                Assert.Equal((69UL, 23UL), p1.ButtonA);
+                Assert.Equal((27UL, 71UL), p1.ButtonB);
                 Assert.Equal((10000000018641UL, 10000000010279UL), p1.Prize);
             });
 
