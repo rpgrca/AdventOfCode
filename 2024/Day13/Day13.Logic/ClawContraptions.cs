9bae631b4379c2001eedff2be2517c9993714ec7
Roberto <rpgrca@gmail.com>
2024-12-13 02:22:08 -0300

    add test, parse input correctly

--- 2024/Day13/Day13.Logic/ClawContraptions.cs
+++ 2024/Day13/Day13.Logic/ClawContraptions.cs
@@ -1,13 +1,56 @@
+
 namespace Day13.Logic;
 
 public class ClawContraptions
 {
     private readonly string _input;
+    private readonly List<((int OffsetX, int OffsetY) ButtonA, (int OffsetX, int OffsetY) ButtonB, (int X, int Y) Prize)> _contraptions;
+
+    public List<((int OffsetX, int OffsetY) ButtonA, (int OffsetX, int OffsetY) ButtonB, (int X, int Y) Prize)> Contraptions => _contraptions;
+    public int Count => _contraptions.Count;
 
     public ClawContraptions(string input)
     {
         _input = input;
+        _contraptions = new();
+        Parse();
     }
 
-    public int Count => 4;
+    private void Parse()
+    {
+        var machines = _input.Split("\n\n");
+        foreach (var machine in machines)
+        {
+            var data = machine.Split('\n').Select(p => p.Split(':'));
+            (int, int) buttonA = default;
+            (int, int) buttonB = default;
+            (int, int) prize = default;
+            foreach (var dat in data)
+            {
+                if (dat[0] == "Button A")
+                {
+                    var info = dat[1].Split(',', StringSplitOptions.TrimEntries)
+                        .Select(p => int.Parse(p[2..]))
+                        .ToList();
+                    buttonA = (info[0], info[1]);
+                }
+                else if (dat[0] == "Button B")
+                {
+                    var info = dat[1].Split(',', StringSplitOptions.TrimEntries)
+                        .Select(p => int.Parse(p[2..]))
+                        .ToList();
+                    buttonB = (info[0], info[1]);
+                }
+                else if (dat[0] == "Prize")
+                {
+                    var info = dat[1].Split(',', StringSplitOptions.TrimEntries)
+                        .Select(p => int.Parse(p[2..]))
+                        .ToList();
+                    prize = (info[0], info[1]);
+                }
+            }
+
+            _contraptions.Add((buttonA, buttonB, prize));
+        }
+    }
 }
--- 2024/Day13/Day13.UnitTests/ClawContraptionsMust.cs
+++ 2024/Day13/Day13.UnitTests/ClawContraptionsMust.cs
@@ -7,10 +7,37 @@ public class ClawContraptionsMust
 {
     [Theory]
     [InlineData(SAMPLE_INPUT, 4)]
-    [InlineData(PUZZLE_INPUT, 100)]
+    [InlineData(PUZZLE_INPUT, 320)]
     public void LoadInputCorrectly(string input, int expectedCount)
     {
         var sut = new ClawContraptions(input);
         Assert.Equal(expectedCount, sut.Count);
+    }
+
+    [Fact]
+    public void ParseInputCorrectly()
+    {
+        var sut = new ClawContraptions(SAMPLE_INPUT);
+        Assert.Collection(sut.Contraptions,
+            p1 => {
+                Assert.Equal((94, 34), p1.ButtonA);
+                Assert.Equal((22, 67), p1.ButtonB);
+                Assert.Equal((8400, 5400), p1.Prize);
+            },
+            p2 => {
+                Assert.Equal((26, 66), p2.ButtonA);
+                Assert.Equal((67, 21), p2.ButtonB);
+                Assert.Equal((12748, 12176), p2.Prize);
+            },
+            p3 => {
+                Assert.Equal((17, 86), p3.ButtonA);
+                Assert.Equal((84, 37), p3.ButtonB);
+                Assert.Equal((7870, 6450), p3.Prize);
+            },
+            p1 => {
+                Assert.Equal((69, 23), p1.ButtonA);
+                Assert.Equal((27, 71), p1.ButtonB);
+                Assert.Equal((18641, 10279), p1.Prize);
+            });
     }
 }
