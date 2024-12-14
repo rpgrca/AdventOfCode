efeea07bd327ca2e2984533b2089679d7d30037a
Roberto <rpgrca@gmail.com>
2024-12-14 11:29:26 -0300

    add tests, move robot correctly

--- 2024/Day14/Day14.Logic/RestroomRedoubt.cs
+++ 2024/Day14/Day14.Logic/RestroomRedoubt.cs
@@ -1,19 +1,96 @@
-
-
-
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Day14.Logic;
+
+[DebuggerDisplay("({Position}, {Velocity})")]
+public class Robot
+{
+    public Coordinates Position;
+    public Coordinates Velocity;
+
+    public Robot(Coordinates position, Coordinates velocity)
+    {
+        Position = position;
+        Velocity = velocity;
+    }
+
+    public override bool Equals([NotNullWhen(true)] object? obj)
+    {
+        if (obj is null) return false;
+        if (obj is Robot other)
+        {
+            return Position.Equals(other.Position) && Velocity.Equals(other.Velocity);
+        }
+
+        return false;
+    }
+
+    public static bool operator == (Robot left, Robot right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator != (Robot left, Robot right)
+    {
+        return !left.Equals(right);
+    }
+
+
+    public override string ToString()
+    {
+        return $"({Position}, {Velocity})";
+    }
+}
+
+public struct Coordinates
+{
+    public int X;
+    public int Y;
+
+    public Coordinates(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public override bool Equals([NotNullWhen(true)] object? obj)
+    {
+        if (obj is null) return false;
+        if (obj is Coordinates other)
+        {
+            return X == other.X && Y == other.Y;
+        }
+
+        return false;
+    }
+
+    public static bool operator == (Coordinates left, Coordinates right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator != (Coordinates left, Coordinates right)
+    {
+        return !left.Equals(right);
+    }
+
+    public override string ToString()
+    {
+        return $"({X}, {Y})";
+    }
+}
 
 public class RestroomRedoubt
 {
     private string _input;
     private int _width;
     private int _height;
-    private readonly List<((int X, int Y) Position, (int X, int Y) Velocity)> _robots;
+    private readonly List<Robot> _robots;
 
     public int RobotCount => _robots.Count;
 
-    public List<((int X, int Y) Position, (int X, int Y) Velocity)> Robots => _robots;
+    public List<Robot> Robots => _robots;
 
     public RestroomRedoubt(string input, int width, int height)
     {
@@ -33,12 +110,25 @@ public class RestroomRedoubt
             var coordinates = line.Split(' ');
             var position = coordinates[0][2..].Split(',').Select(int.Parse).ToArray();
             var velocity = coordinates[1][2..].Split(',').Select(int.Parse).ToArray();
-            _robots.Add(((position[0], position[1]), (velocity[0], velocity[1])));
+            _robots.Add(new(new(position[0], position[1]), new(velocity[0], velocity[1])));
         }
     }
 
-    public void CalculateAfter(int v)
+    public void CalculateAfter(int seconds)
     {
-        throw new NotImplementedException();
+        while (seconds-- > 0)
+        {
+            var robot = _robots[0];
+            robot.Position.X += robot.Velocity.X;
+            if (robot.Position.X >= _width)
+            {
+                robot.Position.X -= _width;
+            }
+            robot.Position.Y += robot.Velocity.Y;
+            if (robot.Position.Y < 0)
+            {
+                robot.Position.Y = _height + robot.Position.Y;
+            }
+        }
     }
 }
--- 2024/Day14/Day14.UnitTests/RestroomRedoubtMust.cs
+++ 2024/Day14/Day14.UnitTests/RestroomRedoubtMust.cs
@@ -1,6 +1,4 @@
-using System.Security.Cryptography;
 using Day14.Logic;
-using NuGet.Frameworks;
 using static Day14.UnitTests.Constants;
 
 namespace Day14.UnitTests;
@@ -20,11 +18,65 @@ public class RestroomRedoubtMust
     public void SetupRobotCorrectly()
     {
         var sut = new RestroomRedoubt("p=2,4 v=2,-3", 11, 7);
+        sut.CalculateAfter(0);
         Assert.Collection(sut.Robots,
             p1 => {
-                Assert.Equal(((2, 4), (2, -3)), p1);
+                Assert.Equal(new(new(2, 4), new(2, -3)), p1);
             });
     }
 
+    [Fact]
+    public void MoveRobotCorrectly_When1SecondElapsed()
+    {
+        var sut = new RestroomRedoubt("p=2,4 v=2,-3", 11, 7);
+        sut.CalculateAfter(1);
+        Assert.Collection(sut.Robots,
+            p1 => {
+                Assert.Equal(new(new(4, 1), new(2, -3)), p1);
+            });
+    }
 
+    [Fact]
+    public void MoveRobotCorrectly_When2SecondsElapsed()
+    {
+        var sut = new RestroomRedoubt("p=2,4 v=2,-3", 11, 7);
+        sut.CalculateAfter(2);
+        Assert.Collection(sut.Robots,
+            p1 => {
+                Assert.Equal(new(new(6, 5), new(2, -3)), p1);
+            });
+    }
+
+    [Fact]
+    public void MoveRobotCorrectly_When3SecondsElapsed()
+    {
+        var sut = new RestroomRedoubt("p=2,4 v=2,-3", 11, 7);
+        sut.CalculateAfter(3);
+        Assert.Collection(sut.Robots,
+            p1 => {
+                Assert.Equal(new(new(8, 2), new(2, -3)), p1);
+            });
+    }
+
+    [Fact]
+    public void MoveRobotCorrectly_When4SecondsElapsed()
+    {
+        var sut = new RestroomRedoubt("p=2,4 v=2,-3", 11, 7);
+        sut.CalculateAfter(4);
+        Assert.Collection(sut.Robots,
+            p1 => {
+                Assert.Equal(new(new(10, 6), new(2, -3)), p1);
+            });
+    }
+
+    [Fact]
+    public void MoveRobotCorrectly_When5SecondsElapsed()
+    {
+        var sut = new RestroomRedoubt("p=2,4 v=2,-3", 11, 7);
+        sut.CalculateAfter(5);
+        Assert.Collection(sut.Robots,
+            p1 => {
+                Assert.Equal(new(new(1, 3), new(2, -3)), p1);
+            });
+    }
 }
