7ffab637e13a3051c43d9421a4ab28c8595de973
Roberto <rpgrca@gmail.com>
2024-12-15 15:22:42 -0300

    initial setup day 15

---
+++ 2024/Day15/Day15.Logic/WarehouseWoes.cs
@@ -0,0 +1,16 @@
+
+namespace Day15.Logic;
+
+public class WarehouseWoes
+{
+    private string _input;
+
+    public WarehouseWoes(string input)
+    {
+        _input = input;
+    }
+
+    public int Size { get; set; } = 10;
+
+    public int Count { get;set;} = 700;
+}
---
+++ 2024/Day15/Day15.UnitTests/WarehouseWoesMust.cs
@@ -0,0 +1,16 @@
+using Day15.Logic;
+using static Day15.UnitTests.Constants;
+
+namespace Day15.UnitTests;
+
+public class WarehouseWoesMust
+{
+    [Theory]
+    [InlineData(SAMPLE_INPUT, 10, 700)]
+    public void LoadInputCorrectly(string input, int expectedSize, int expectedMoves)
+    {
+        var sut = new WarehouseWoes(input);
+        Assert.Equal(expectedSize, sut.Size);
+        Assert.Equal(expectedMoves, sut.Count);
+    }
+}
