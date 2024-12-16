b41c48ca2c8368522b8eb80798be1df7cbf30863
Roberto <rpgrca@gmail.com>
2024-12-16 19:02:54 -0300

    add test, load input correctly

--- 2024/Day16/Day16.Logic/ReindeerMaze.cs
+++ 2024/Day16/Day16.Logic/ReindeerMaze.cs
@@ -1,6 +1,16 @@
+
 namespace Day16.Logic;
 
-public class Class1
+public class ReindeerMaze
 {
+    private string _input;
+    private string[] _lines;
 
+    public int Size => _lines.Length;
+
+    public ReindeerMaze(string input)
+    {
+        _input = input;
+        _lines = _input.Split('\n');
+    }
 }
--- 2024/Day16/Day16.UnitTests/ReindeerMazeMust.cs
+++ 2024/Day16/Day16.UnitTests/ReindeerMazeMust.cs
@@ -5,9 +5,13 @@ namespace Day16.UnitTests;
 
 public class ReindeerMazeMust
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData(SAMPLE_INPUT, 15)]
+    [InlineData(SECOND_SAMPLE_INPUT, 17)]
+    [InlineData(PUZZLE_INPUT, 141)]
+    public void LoadInputCorrectly(string input, int expectedSize)
     {
-
+        var sut = new ReindeerMaze(input);
+        Assert.Equal(expectedSize, sut.Size);
     }
 }
