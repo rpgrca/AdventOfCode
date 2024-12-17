b1c5a7073483dd752e82fb86da238b3c9b14a23a
Roberto <rpgrca@gmail.com>
2024-12-17 10:57:54 -0300

    add test, load input correctly

--- 2024/Day17/Day17.Logic/ChronospatialComputer.cs
+++ 2024/Day17/Day17.Logic/ChronospatialComputer.cs
@@ -1,6 +1,32 @@
+
 namespace Day17.Logic;
 
 public class ChronospatialComputer
 {
+    private readonly string _input;
+    private readonly List<int> _program;
 
+    public ChronospatialComputer(string input)
+    {
+        _input = input;
+        var sections = _input.Split("\n\n");
+        foreach (var section in sections[0].Split('\n'))
+        {
+            var words = section.Split(':', StringSplitOptions.TrimEntries);
+            switch (words[0][^1])
+            {
+                case 'A': A = long.Parse(words[1]); break;
+                case 'B': B = long.Parse(words[1]); break;
+                case 'C': C = long.Parse(words[1]); break;
+            }
+        }
+
+        var programs = sections[1].Split(':', StringSplitOptions.TrimEntries);
+        _program = programs[1].Split(',').Select(int.Parse).ToList();
+    }
+
+    public long A { get; private set; }
+    public long B { get; private set; }
+    public long C { get; private set; }
+    public int Length => _program.Count;
 }
--- 2024/Day17/Day17.UnitTests/ChronospatialComputerMust.cs
+++ 2024/Day17/Day17.UnitTests/ChronospatialComputerMust.cs
@@ -5,9 +5,15 @@ namespace Day17.UnitTests;
 
 public class ChronospatialComputerMust
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData(SAMPLE_INPUT, 729, 0, 0, 6)]
+    [InlineData(PUZZLE_INPUT, 63281501, 0, 0, 16)]
+    public void LoadInputCorrectly(string input, int expectedA, int expectedB, int expectedC, int expectedLength)
     {
-
+        var sut = new ChronospatialComputer(input);
+        Assert.Equal(expectedA, sut.A);
+        Assert.Equal(expectedB, sut.B);
+        Assert.Equal(expectedC, sut.C);
+        Assert.Equal(expectedLength, sut.Length);
     }
 }
