2fba190793457a1f09f44e6a5cc2f8cbbbd4f230
Roberto <rpgrca@gmail.com>
2024-12-19 11:15:31 -0300

    add test, load input correctly

--- 2024/Day19/Day19.Logic/LinenLayout.cs
+++ 2024/Day19/Day19.Logic/LinenLayout.cs
@@ -1,6 +1,22 @@
+
+
 namespace Day19.Logic;
 
 public class LinenLayout
 {
+    private readonly string _input;
+    private List<string> _towels;
+    private List<string> _designs;
+
+    public int DesignsCount => _designs.Count;
+    public int TowelsCount => _towels.Count;
+
+    public LinenLayout(string input)
+    {
+        _input = input;
+        var sections = _input.Split("\n\n");
+        _towels = sections[0].Split(',', StringSplitOptions.TrimEntries).ToList();
+        _designs = sections[1].Split('\n').ToList();
+    }
 
 }
--- 2024/Day19/Day19.UnitTests/LinenLayoutMust.cs
+++ 2024/Day19/Day19.UnitTests/LinenLayoutMust.cs
@@ -5,9 +5,13 @@ namespace Day19.UnitTests;
 
 public class LinenLayoutMust
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData(SAMPLE_INPUT, 8, 8)]
+    [InlineData(PUZZLE_INPUT, 447, 400)]
+    public void LoadInputCorrectly(string input, int expectedTowels, int expectedDesigns)
     {
-
+        var sut = new LinenLayout(input);
+        Assert.Equal(expectedTowels, sut.TowelsCount);
+        Assert.Equal(expectedDesigns, sut.DesignsCount);
     }
 }
