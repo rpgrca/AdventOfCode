9097ab5fbf21d8e27ca1968d09a1cc4231780487
Roberto <rpgrca@gmail.com>
2024-12-18 22:21:41 -0300

    initial setup day 18

---
+++ 2024/Day18/Day18.Logic/RamRun.cs
@@ -0,0 +1,6 @@
+namespace Day18.Logic;
+
+public class RamRun
+{
+
+}
---
+++ 2024/Day18/Day18.UnitTests/RamRunMust.cs
@@ -0,0 +1,13 @@
+using Day18.Logic;
+using static Day18.UnitTests.Constants;
+
+namespace Day18.UnitTests;
+
+public class RamRunMust
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
