c6cdeab828010f570f568873ef4fc05ad3887f96
Roberto <rpgrca@gmail.com>
2024-12-18 22:39:32 -0300

    add test, load corrupted memory in map correctly

--- 2024/Day18/Day18.Logic/RamRun.cs
+++ 2024/Day18/Day18.Logic/RamRun.cs
@@ -1,3 +1,4 @@
+using System.Text;
 
 namespace Day18.Logic;
 
@@ -8,11 +9,44 @@ public class RamRun
 
     public int Count => _corruptedMemory.Count;
 
-    public RamRun(string input)
+    public int Size { get; private set; }
+    private readonly char[,] _memoryMap;
+
+    public RamRun(string input, int size)
     {
         _input = input;
+        Size = size;
+        _memoryMap = new char[Size,Size];
+        for (var y = 0; y < Size; y++)
+            for (var x = 0; x < Size; x++)
+                _memoryMap[y,x] = '.';
+
         _corruptedMemory = _input.Split('\n')
             .Select(p => (int.Parse(p.Split(',')[0]), int.Parse(p.Split(',')[1])))
             .ToList();
+    }
+
+    public void Load(int bytes)
+    {
+        foreach (var corruptedMemory in _corruptedMemory.Take(bytes))
+        {
+            _memoryMap[corruptedMemory.Y, corruptedMemory.X] = '#';
+        }
+    }
+
+    public string Plot()
+    {
+        var sb = new StringBuilder();
+        for (var y = 0; y < Size; y++)
+        {
+            for (var x = 0; x < Size; x++)
+            {
+                sb.Append(_memoryMap[y, x]);
+            }
+
+            sb.AppendLine();
+        }
+
+        return sb.ToString().Trim();
     }
 }
--- 2024/Day18/Day18.UnitTests/RamRunMust.cs
+++ 2024/Day18/Day18.UnitTests/RamRunMust.cs
@@ -6,11 +6,26 @@ namespace Day18.UnitTests;
 public class RamRunMust
 {
     [Theory]
-    [InlineData(SAMPLE_INPUT, 25)]
-    [InlineData(PUZZLE_INPUT, 3450)]
-    public void LoadInputCorrectly(string input, int expectedCount)
+    [InlineData(SAMPLE_INPUT, 25, 7)]
+    [InlineData(PUZZLE_INPUT, 3450, 71)]
+    public void LoadInputCorrectly(string input, int expectedCount, int expectedSize)
     {
-        var sut = new RamRun(input);
+        var sut = new RamRun(input, expectedSize);
         Assert.Equal(expectedCount, sut.Count);
+        Assert.Equal(expectedSize, sut.Size);
+    }
+
+    [Fact]
+    public void LoadCorruptedMemoryInMapCorrectly()
+    {
+        var sut = new RamRun(SAMPLE_INPUT, 7);
+        sut.Load(12);
+        Assert.Equal(@"...#...
+..#..#.
+....#..
+...#..#
+..#..#.
+.#..#..
+#.#....", sut.Plot());
     }
 }
