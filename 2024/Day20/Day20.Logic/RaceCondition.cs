785e5cb77ac52b65bc7b3d442e7097dacff8b262
Roberto <rpgrca@gmail.com>
2024-12-20 11:50:33 -0300

    add test, check start and end position

--- 2024/Day20/Day20.Logic/RaceCondition.cs
+++ 2024/Day20/Day20.Logic/RaceCondition.cs
@@ -1,16 +1,44 @@
 
+
 namespace Day20.Logic;
 
 public class RaceCondition
 {
     private readonly string _input;
     private readonly string[] _lines;
+    private char[,] _map;
 
     public int Size => _lines.Length;
+
+    public (int X, int Y) Start { get; set; }
+    public (int X, int Y) End { get; set; }
 
     public RaceCondition(string input)
     {
         _input = input;
         _lines = _input.Split('\n').ToArray();
+        _map = new char[Size, Size];
+        for (var y = 0; y < Size; y++)
+        {
+            for (var x = 0; x < Size; x++)
+            {
+                switch (_lines[y][x])
+                {
+                    case 'S':
+                        Start = (x, y);
+                        _map[y,x] = '.';
+                        break;
+
+                    case 'E':
+                        End = (x, y);
+                        _map[y,x] = '.';
+                        break;
+
+                    default:
+                        _map[y, x] = _lines[y][x];
+                        break;
+                }
+            }
+        }
     }
 }
--- 2024/Day20/Day20.UnitTests/RaceConditionMust.cs
+++ 2024/Day20/Day20.UnitTests/RaceConditionMust.cs
@@ -6,11 +6,13 @@ namespace Day20.UnitTests;
 public class RaceConditionMust
 {
     [Theory]
-    [InlineData(SAMPLE_INPUT, 15)]
-    [InlineData(PUZZLE_INPUT, 141)]
-    public void LoadInputCorrectly(string input, int expectedSize)
+    [InlineData(SAMPLE_INPUT, 15, 1, 3, 5, 7)]
+    [InlineData(PUZZLE_INPUT, 141, 43, 73, 69, 71)]
+    public void LoadInputCorrectly(string input, int expectedSize, int startX, int startY, int endX, int endY)
     {
         var sut = new RaceCondition(input);
         Assert.Equal(expectedSize, sut.Size);
+        Assert.Equal((startX, startY), sut.Start);
+        Assert.Equal((endX, endY), sut.End);
     }
 }
