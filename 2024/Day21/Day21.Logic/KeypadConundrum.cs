4a85f52cb6ec07779a235e95deab149e127cd821
Roberto <rpgrca@gmail.com>
2024-12-21 11:23:02 -0300

    add test, load input correctly

--- 2024/Day21/Day21.Logic/KeypadConundrum.cs
+++ 2024/Day21/Day21.Logic/KeypadConundrum.cs
@@ -1,6 +1,16 @@
+
 namespace Day21.Logic;
 
 public class KeypadConundrum
 {
+    private string _input;
+
+    public List<int> Codes { get; set; }
+
+    public KeypadConundrum(string input)
+    {
+        _input = input;
+        Codes = _input.Split('\n').Select(p => int.Parse(p[0..^1])).ToList();
+    }
 
 }
--- 2024/Day21/Day21.UnitTests/KeypadConundrumMust.cs
+++ 2024/Day21/Day21.UnitTests/KeypadConundrumMust.cs
@@ -5,9 +5,17 @@ namespace Day21.UnitTests;
 
 public class KeypadConundrumMust
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData(SAMPLE_INPUT, 29, 980, 179, 456, 379)]
+    [InlineData(PUZZLE_INPUT, 789, 540, 285, 140, 189)]
+    public void LoadInputCorrectly(string input, int expectedA, int expectedB, int expectedC, int expectedD, int expectedE)
     {
-
+        var sut = new KeypadConundrum(input);
+        Assert.Collection(sut.Codes,
+            p1 => Assert.Equal(expectedA, p1),
+            p2 => Assert.Equal(expectedB, p2),
+            p3 => Assert.Equal(expectedC, p3),
+            p4 => Assert.Equal(expectedD, p4),
+            p5 => Assert.Equal(expectedE, p5));
     }
 }
