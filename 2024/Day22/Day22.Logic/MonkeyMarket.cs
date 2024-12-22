f0d1437f6bbf8eb8e8399ab33bdcaa2fd9fd41ec
Roberto <rpgrca@gmail.com>
2024-12-22 10:03:54 -0300

    add test, generate secret correctly

--- 2024/Day22/Day22.Logic/MonkeyMarket.cs
+++ 2024/Day22/Day22.Logic/MonkeyMarket.cs
@@ -1,17 +1,44 @@
-using System.Collections.Generic;
+
+using System.Reflection.Metadata;
 
 namespace Day22.Logic;
 
 public class MonkeyMarket
 {
+    private const int PruneNumber = 16777216;
     private string _input;
-    private readonly List<int> _list;
+    private readonly List<long> _list;
+    private readonly long[] _currentPrice;
+    private readonly long[] _sums;
 
     public int Count => _list.Count;
+
+    public long SumOfSecrets { get; private set; }
 
     public MonkeyMarket(string input)
     {
         _input = input;
-        _list = _input.Split('\n').Select(int.Parse).ToList();
+        _list = _input.Split('\n').Select(long.Parse).ToList();
+        _currentPrice = _list.ToArray();
+        _sums = new long[Count];
+    }
+
+    public void Generate(int count)
+    {
+        while (count-- > 0)
+        {
+            for (var index = 0; index < Count; index++)
+            {
+                // 1000000000000000000000000
+                var secret = _currentPrice[index];
+                secret = ((secret * 64) ^ secret) % PruneNumber;
+                secret = ((secret / 32) ^ secret) % PruneNumber;
+                secret = ((secret * 2048) ^ secret) % PruneNumber;
+                _currentPrice[index] = secret;
+                _sums[index] += secret;
+            }
+        }
+
+        SumOfSecrets = _sums.Sum();
     }
 }
--- 2024/Day22/Day22.UnitTests/MonkeyMarketMust.cs
+++ 2024/Day22/Day22.UnitTests/MonkeyMarketMust.cs
@@ -13,4 +13,22 @@ public class MonkeyMarketMust
         var sut = new MonkeyMarket(input);
         Assert.Equal(expectedCount, sut.Count);
     }
+
+    [Theory]
+    [InlineData(1, 15887950)]
+    [InlineData(2, 32383086)]
+    [InlineData(3, 32910431)]
+    [InlineData(4, 33614955)]
+    [InlineData(5, 35168639)]
+    [InlineData(6, 47851795)]
+    [InlineData(7, 58952339)]
+    [InlineData(8, 71201823)]
+    [InlineData(9, 78955255)]
+    [InlineData(10, 84863509)]
+    public void CalculateSumOfSecrets(int generations, int expectedResult)
+    {
+        var sut = new MonkeyMarket("123");
+        sut.Generate(generations);
+        Assert.Equal(expectedResult, sut.SumOfSecrets);
+    }
 }
