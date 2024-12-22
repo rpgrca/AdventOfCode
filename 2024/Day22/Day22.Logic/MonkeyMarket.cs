77c2e05f7b8c46334764fa5a8308dc013ecdb8d8
Roberto <rpgrca@gmail.com>
2024-12-22 11:19:22 -0300

    add test, calculate change correctly

--- 2024/Day22/Day22.Logic/MonkeyMarket.cs
+++ 2024/Day22/Day22.Logic/MonkeyMarket.cs
@@ -1,20 +1,46 @@
 
+
 namespace Day22.Logic;
 
 public class MonkeyMarket
 {
     private const int PruneNumber = 16777216;
     private string _input;
-    private readonly List<long> _list;
+    private readonly List<long> _buyers;
+    private readonly List<long>[] _secrets;
+    private readonly List<long>[] _changes;
+    private readonly List<long>[] _prices;
 
-    public int Count => _list.Count;
+    public int Count => _buyers.Count;
 
     public long SumOfSecrets { get; private set; }
 
-    public MonkeyMarket(string input)
+    public MonkeyMarket(string input, int transactions)
     {
         _input = input;
-        _list = _input.Split('\n').Select(long.Parse).ToList();
+        _buyers = _input.Split('\n').Select(long.Parse).ToList();
+        _secrets = _buyers.Select(p => new List<long>() { p }).ToArray();
+        _prices = _secrets.Select(p => new List<long>() { p[0] % 10 }).ToArray();
+        _changes = _buyers.Select(p => new List<long>() { 0 }).ToArray();
+
+        for (var index = 0; index < Count; index++)
+        {
+            var secret = _buyers[index];
+            var lastPrice = _prices[index][0];
+
+            for (var currentTransaction = 0; currentTransaction < transactions; currentTransaction++)
+            {
+                secret = ((secret << 6) ^ secret) % PruneNumber;
+                secret = ((secret >> 5) ^ secret) % PruneNumber;
+                secret = ((secret << 11) ^ secret) % PruneNumber;
+
+                _secrets[index].Add(secret);
+                var price = secret % 10;
+                _prices[index].Add(price);
+                _changes[index].Add(price - lastPrice);
+                lastPrice = price;
+            }
+        }
     }
 
     public void CalculateSumOfSecrets(int count)
@@ -28,17 +54,20 @@ public class MonkeyMarket
 
     public long Generate(int index, int count)
     {
-        var secret = _list[index];
+        var secret = _buyers[index];
 
         while (count-- > 0)
         {
-            secret = ((secret * 64) ^ secret) % PruneNumber;
-            secret = ((secret / 32) ^ secret) % PruneNumber;
-            secret = ((secret * 2048) ^ secret) % PruneNumber;
+            secret = ((secret << 6) ^ secret) % PruneNumber;
+            secret = ((secret >> 5) ^ secret) % PruneNumber;
+            secret = ((secret << 11) ^ secret) % PruneNumber;
         }
 
         return secret;
     }
 
     public long GeneratePrice(int index, int count) => Generate(index, count) % 10;
+
+    public long CalculateChange(int index, int count) =>
+        count == 0? throw new ArgumentException($"Count cannot be zero") : _changes[index][count];
 }
--- 2024/Day22/Day22.UnitTests/MonkeyMarketMust.cs
+++ 2024/Day22/Day22.UnitTests/MonkeyMarketMust.cs
@@ -10,7 +10,7 @@ public class MonkeyMarketMust
     [InlineData(PUZZLE_INPUT, 2032)]
     public void LoadInputCorrectly(string input, int expectedCount)
     {
-        var sut = new MonkeyMarket(input);
+        var sut = new MonkeyMarket(input, 0);
         Assert.Equal(expectedCount, sut.Count);
     }
 
@@ -27,7 +27,7 @@ public class MonkeyMarketMust
     [InlineData(10, 5908254)]
     public void CalculateSumOfSecrets(int generations, int expectedResult)
     {
-        var sut = new MonkeyMarket("123");
+        var sut = new MonkeyMarket("123", generations);
         var result = sut.Generate(0, generations);
         Assert.Equal(expectedResult, result);
     }
@@ -39,7 +39,7 @@ public class MonkeyMarketMust
     [InlineData(2024, 8667524)]
     public void Calculate2000thSecretNumber(int initial, int expectedValue)
     {
-        var sut = new MonkeyMarket(initial.ToString());
+        var sut = new MonkeyMarket(initial.ToString(), 2000);
         var value = sut.Generate(0, 2000);
         Assert.Equal(expectedValue, value);
     }
@@ -47,7 +47,7 @@ public class MonkeyMarketMust
     [Fact]
     public void SolveFirstSampleCorrectly()
     {
-        var sut = new MonkeyMarket(SAMPLE_INPUT);
+        var sut = new MonkeyMarket(SAMPLE_INPUT, 2000);
         sut.CalculateSumOfSecrets(2000);
         Assert.Equal(37327623, sut.SumOfSecrets);
     }
@@ -55,7 +55,7 @@ public class MonkeyMarketMust
     [Fact]
     public void SolveFirstPuzzleCorrectly()
     {
-        var sut = new MonkeyMarket(PUZZLE_INPUT);
+        var sut = new MonkeyMarket(PUZZLE_INPUT, 2000);
         sut.CalculateSumOfSecrets(2000);
         Assert.True(67070480906 > sut.SumOfSecrets);
         Assert.Equal(16999668565, sut.SumOfSecrets);
@@ -74,8 +74,18 @@ public class MonkeyMarketMust
     [InlineData(9, 2)]
     public void CalculatePricesCorrectly(int count, int expectedPrice)
     {
-        var sut = new MonkeyMarket("123");
+        var sut = new MonkeyMarket("123", count);
         var result = sut.GeneratePrice(0, count);
         Assert.Equal(expectedPrice, result);
+    }
+
+    [Theory]
+    [InlineData(1, -3)]
+    [InlineData(2, 6)]
+    public void CalculatePriceChangesCorrectly(int changeNumber, int expectedChange)
+    {
+        var sut = new MonkeyMarket("123", 10);
+        var result = sut.CalculateChange(0, changeNumber);
+        Assert.Equal(expectedChange, result);
     }
 }
