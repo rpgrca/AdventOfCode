f4ca3109d967fd05cf345a49c7b65cc453007b36
Roberto <rpgrca@gmail.com>
2024-12-23 11:03:09 -0300

    add test, find groups of 3 computers

--- 2024/Day23/Day23.Logic/LanParty.cs
+++ 2024/Day23/Day23.Logic/LanParty.cs
@@ -1,9 +1,13 @@
+
+using System.Net.Sockets;
+
 namespace Day23.Logic;
 
 public class LanParty
 {
     private readonly string _input;
     private readonly string[] _lines;
+    private readonly Dictionary<string, List<string>> _connections;
 
     public int Count => _lines.Length;
 
@@ -11,5 +15,46 @@ public class LanParty
     {
         _input = input;
         _lines = _input.Split('\n');
+
+        _connections = new();
+        foreach (var line in _lines)
+        {
+            var computers = line.Split('-');
+            _connections.TryAdd(computers[0], new());
+            _connections.TryAdd(computers[1], new());
+            _connections[computers[0]].Add(computers[1]);
+            _connections[computers[1]].Add(computers[0]);
+        }
+    }
+
+    public List<string> FindPartiesOf(int size)
+    {
+        /*
+        var partiesOfSize = _connections
+            .Where(p => p.Value.Count == size - 1)
+            .Select(p => new List<string>(p.Value) { p.Key }.Order())
+            .GroupBy(p => p)
+            .Where(p => p.Count() == size)
+            .Select(p => $"{p.First()}-{p.Skip(1).Take(1)}-{p.Skip(2).Take(1)}")
+            .ToList();*/
+
+        var result = new List<string>();
+        foreach (var computer in _connections)
+        {
+            if (computer.Value.Count >= size)
+            {
+                foreach (var other in computer.Value)
+                {
+                    var othersConnectionsToOriginal = _connections[other].Where(p => _connections[p].Contains(computer.Key)).ToList();
+                    foreach (var otherConnectionToOriginal in othersConnectionsToOriginal)
+                    {
+                        var network = string.Join(',', new List<string> { computer.Key, other, otherConnectionToOriginal }.Order());
+                        result.Add(network);
+                    }
+                }
+            }
+        }
+
+        return result.Distinct().Order().ToList();
     }
 }
--- 2024/Day23/Day23.UnitTests/LanPartyMust.cs
+++ 2024/Day23/Day23.UnitTests/LanPartyMust.cs
@@ -13,4 +13,24 @@ public class LanPartyMust
         var sut = new LanParty(input);
         Assert.Equal(expectedCount, sut.Count);
     }
+
+    [Fact]
+    public void FindThreeComputerPartiesCorrectly()
+    {
+        var sut = new LanParty(SAMPLE_INPUT);
+        var list = sut.FindPartiesOf(3);
+        Assert.Collection(list,
+            p1 => Assert.Equal("aq,cg,yn", p1),
+            p1 => Assert.Equal("aq,vc,wq", p1),
+            p1 => Assert.Equal("co,de,ka", p1),
+            p1 => Assert.Equal("co,de,ta", p1),
+            p1 => Assert.Equal("co,ka,ta", p1),
+            p1 => Assert.Equal("de,ka,ta", p1),
+            p1 => Assert.Equal("kh,qp,ub", p1),
+            p1 => Assert.Equal("qp,td,wh", p1),
+            p1 => Assert.Equal("tb,vc,wq", p1),
+            p1 => Assert.Equal("tc,td,wh", p1),
+            p1 => Assert.Equal("td,wh,yn", p1),
+            p1 => Assert.Equal("ub,vc,wq", p1));
+    }
 }
