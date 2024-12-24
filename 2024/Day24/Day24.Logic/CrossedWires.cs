3a92039310765a360847b4d74214713b83eb8c69
Roberto <rpgrca@gmail.com>
2024-12-24 10:48:49 -0300

    add test, load input correctly

--- 2024/Day24/Day24.Logic/CrossedWires.cs
+++ 2024/Day24/Day24.Logic/CrossedWires.cs
@@ -1,6 +1,21 @@
+
 namespace Day24.Logic;
 
 public class CrossedWires
 {
+    private string _input;
+    private string[] _wires;
+    private string[] _gates;
+
+    public int GatesCount => _gates.Length;
+    public int WiresCount => _wires.Length;
+
+    public CrossedWires(string input)
+    {
+        _input = input;
+        var sections = _input.Split("\n\n");
+        _wires = sections[0].Split('\n');
+        _gates = sections[1].Split('\n');
+    }
 
 }
--- 2024/Day24/Day24.UnitTests/CrossedWiresMust.cs
+++ 2024/Day24/Day24.UnitTests/CrossedWiresMust.cs
@@ -1,10 +1,18 @@
+using Day24.Logic;
+using static Day24.UnitTests.Constants;
+
 namespace Day24.UnitTests;
 
 public class CrossedWiredMust
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData(SAMPLE_INPUT, 10, 36)]
+    [InlineData(SECOND_SAMPLE_INPUT, 6, 3)]
+    [InlineData(PUZZLE_INPUT, 90, 222)]
+    public void LoadInputCorrectly(string input, int expectedWires, int expectedGates)
     {
-
+        var sut = new CrossedWires(input);
+        Assert.Equal(expectedWires, sut.WiresCount);
+        Assert.Equal(expectedGates, sut.GatesCount);
     }
 }
